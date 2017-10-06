f78e864b287f50a2a1ea7b6670524f9afda69eba
Eric <theadamsfamily11@msn.com>
2017-10-06 11:33:14 -0700

    Python Push Ups

---
+++ session01/break_me.py
@@ -0,0 +1,35 @@
+"""
+uncomment name_error() to get a NameError
+uncomment type_error() to get a TypeError
+uncomment syntax_error() and uncomment the print statement to get a SyntaxError
+uncomment attribute_error() to get an AttributeError
+"""
+
+# name_error()
+
+def name_error():
+	pass
+
+def type_error():
+	x = 1
+	y = "1"
+	z = x+y
+	z
+
+def syntax_error():
+	pass
+	# print "Syntax Error!"
+
+def attribute_error():
+	x = 7
+	x.append(11)
+
+
+# type_error()
+
+# syntax_error()
+
+# attribute_error()
+
+
+
