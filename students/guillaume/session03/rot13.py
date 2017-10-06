5f93a6a3ce87470e5d0af2bd61f4fc7f1d1be98d
Guillaume THOMAS <guillaumethomas@outlook.com>
2017-10-05 22:43:38 -0700

    more general & correction for python3 library str

--- students/guillaume/session03/rot13.py
+++ students/guillaume/session03/rot13.py
@@ -1,28 +1,47 @@
 
 
-from string import ascii_lowercase, ascii_uppercase, maketrans
+from string import ascii_lowercase, ascii_uppercase
 
-n = 13
-lower_case = ascii_lowercase
-upper_case = ascii_uppercase
-std_alphabet = lower_case + upper_case
 
-rot_lower = lower_case[n:] + lower_case[:n] 
-rot_upper = upper_case[n:] + upper_case[:n]
-rot13_alphabet = rot_lower + rot_upper
+def rot(n):
+	'''
+	Generate subsitution cypher table of value n
+	'''
+	lower_case = ascii_lowercase
+	upper_case = ascii_uppercase
+	rot_lower = lower_case[n:] + lower_case[:n] 
+	rot_upper = upper_case[n:] + upper_case[:n]
+
+	std_alphabet = lower_case + upper_case
+	rot13_alphabet = rot_lower + rot_upper
+
+	return std_alphabet, rot13_alphabet
+
+def translation(string, n, boolean = True):
+	'''
+	Provide:
+	- translation from natural to encoded by default
+	- translation from encoded to natural when boolean == False
+	'''
+	from_t, to_t = rot(n)
+	if boolean == False: from_t, to_t = to_t, from_t
+	trans_table = str.maketrans(from_t, to_t)
+	return string.translate(trans_table)
 
 def rot13(string):
-	
-	trans_table = maketrans(std_alphabet, rot13_alphabet)
-	return string.translate(trans_table)
+	'''
+	Provide translation from natural to encoded
+	'''
+	return translation(string, 13)
 
 def rot13_reverse(string):
-	
-	trans_table = maketrans(rot13_alphabet, std_alphabet)
-	return string.translate(trans_table)
+	'''
+	Provide translation from encoded to natural
+	'''
+	return translation(string, 13, False)
 
 if __name__ == '__main__':
-	print(std_alphabet)
-	print(rot13_alphabet)
+	print(rot(13))
 	print(rot13('testo'))
 	print(rot13_reverse('Zntargvp sebz bhgfvqr arne pbeare'))
+	
