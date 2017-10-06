608b3dd7d6c97d5f8ff7b0730039e30216d9cab6
Kathryn Egan <kathryn.s.nichols@gmail.com>
2017-10-06 09:38:20 -0700

    converted tabs to spaces :(

--- students/kegan/session01/break_me.py
+++ students/kegan/session01/break_me.py
@@ -9,35 +9,35 @@ SyntaxError code included and commented out.
 
 
 def cause_name_error():
-	try:
-		print(name)
-	except NameError:
-		print('Successful NameError')
-		return
-	print('No NameError')
+    try:
+        print(name)
+    except NameError:
+        print('Successful NameError')
+        return
+    print('No NameError')
 
 
 def cause_type_error():
-	try:
-		string = 'string'
-		string += 1
-	except TypeError:
-		print('Successful TypeError')
-		return
-	print('No TypeError')
+    try:
+        string = 'string'
+        string += 1
+    except TypeError:
+        print('Successful TypeError')
+        return
+    print('No TypeError')
 
 
 def cause_attribute_error():
-	class ErrorProne:
-		def __init__(self):
-			self.value1 = 1
-	try:
-		error = ErrorProne()
-		print(error.value2)
-	except AttributeError:
-		print('Successful AttributeError')
-		return
-	print('No AttributeError')
+    class ErrorProne:
+        def __init__(self):
+            self.value1 = 1
+    try:
+        error = ErrorProne()
+        print(error.value2)
+    except AttributeError:
+        print('Successful AttributeError')
+        return
+    print('No AttributeError')
 
 
 cause_name_error()
@@ -46,4 +46,4 @@ cause_attribute_error()
 
 # cause syntax error
 # while True
-# 	print('broken')
+#     print('broken')
