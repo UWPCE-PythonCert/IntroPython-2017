dc0c64200317edccf0a64e1feb52e9475df3788d
Dan Kuchan <kuchandw@gmail.com>
2017-10-07 15:52:04 -0700

    Added initial information

---
+++ students/Dkuchan/session01/break_me.py
@@ -0,0 +1,10 @@
+
+
+#typeerror occurs when you try to do something to a type that cannot do it IE adding characters or strings 
+
+#syntaxerror raises when an error in syntax is discovered, is this just like a missplaced semicolon?
+
+#nameerror
+
+
+#attributeerror 
