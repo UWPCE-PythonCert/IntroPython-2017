943104914181cb50e144d2f5e8ed70ab8ecd3fdc
Chris Barker <PythonCHB@gmail.com>
2017-10-09 23:07:16 -0700

    fixed a couple typos

--- solutions/Session02/fizz_buzz.py
+++ solutions/Session02/fizz_buzz.py
@@ -11,9 +11,9 @@ def fizzbuzz1(n):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
-            print ("Fizz")
+            print("Fizz")
         elif i % 5 == 0:
-            print ("Buzz")
+            print("Buzz")
         else:
             print(i)
 
