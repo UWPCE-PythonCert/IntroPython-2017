d96b8ecd99c9cc183c1e92dc84cebbfa2b38cace
Brian Nagata <btnagata@gmail.com>
2017-10-10 18:20:55 -0700

    adding break_me content

---
+++ students/briannagata/session01/break_me.py
@@ -0,0 +1,22 @@
+# Python pushups module.
+# Python 3.6
+
+
+def name_error():
+    """ Raises exception NameError """
+    raise NameError
+
+
+def type_error():
+    """ Raises exception TypeError """
+    raise TypeError
+
+
+def syntax_error():
+    """ Raises exception SyntaxError """
+    raise SyntaxError
+
+
+def attribute_error():
+    """ Raises exception AttributeError """
+    raise AttributeError
