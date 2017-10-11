44b39f03cf26221792649d4eb058b8d19c12319d
Dan Kuchan <kuchandw@gmail.com>
2017-10-10 20:58:16 -0700

    Completed Program

--- students/Dkuchan/session02/Fibannacci.py
+++ students/Dkuchan/session02/Fibannacci.py
@@ -1,2 +1,26 @@
 #Fibannacci...
 #I probably spelled that wrong...
+
+
+while True:
+    try:
+        strnglngth=input("Please enter how many numbers you would like resolved: ")
+        strnglngth=int(strnglngth)
+    except ValueError:
+        print("Please enter a Number for string length")
+        continue
+    else:
+	   	break
+	
+def dofibs(strnglngth):
+    print(1,end="")
+    print(',',end="")
+    for count in range(1,strnglngth):
+        print(count+(count-1),end="")
+        if count<strnglngth-1:
+            print(',',end="")
+
+
+dofibs(strnglngth)
+print()
+
