fb05a45945d859bff7ffa91b876c5258d9ad24d4
Guillaume THOMAS <guillaumethomas@outlook.com>
2017-10-10 20:54:27 -0700

    synthax

--- students/guillaume/session03/list_lab.py
+++ students/guillaume/session03/list_lab.py
@@ -1,67 +1,73 @@
 #!/usr/bin/env python3
 
+
+def rev_str(str):
+    return str[::-1]
+
+
 def fruit_lst():
-	'''
-	Playing with list and fruit
-	'''
-	fruits = ['Apple', 'Pears', 'Orange','Peaches']
-	print(fruits)
+    '''
+    Playing with list and fruit
+    '''
+    fruits = ['Apple', 'Pears', 'Orange', 'Peaches']
+    print(fruits)
 
-	new_fruit = input('Please, add a fruit to the list\n')
-	fruits.append(new_fruit)
-	print(fruits)
+    new_fruit = input('Please, add a fruit to the list\n')
+    fruits.append(new_fruit)
+    print(fruits)
 
-	
-	bool = True
-	nb_fruits = len(fruits)
-	while bool:
-		number = input('Please give a number between 1 and {}\n'.format(nb_fruits))
-		try:
-			number = int(number)
-			if number <= nb_fruits:
-				number -= 1
-				bool = False
-			else:
-				print('Not enough fruits!\n')
-		except:
-			print('Wrong input !\n')
+    bool = True
+    nb_fruits = len(fruits)
+    while bool:
+        number = input('Please give a number between 1 and {}\n'.format(nb_fruits))
+        try:
+            number = int(number)
+            if number <= nb_fruits:
+                number -= 1
+                bool = False
+            else:
+                print('Not enough fruits!\n')
+        except:
+            print('Wrong input !\n')
 
-	print( '{} is fruit {}\n'.format(str(number + 1), fruits[number]) )
+    print( '{} is fruit {}\n'.format(str(number + 1), fruits[number]) )
 
-	fruits = ['Raspberry'] + fruits
-	print(fruits)
+    fruits = ['Raspberry'] + fruits
+    print(fruits)
 
-	fruits.insert(0, 'Strawberry')
-	print(fruits)
+    fruits.insert(0, 'Strawberry')
+    print(fruits)
 
-	for fruit in fruits:
-		if fruit[0] == 'P':
-			print(fruit)
+    for fruit in fruits:
+        if fruit[0] == 'P':
+            print(fruit)
 
-	print(fruits)
-	del fruits[-1]
-	print(fruits)
+    print(fruits)
+    del fruits[-1]
+    print(fruits)
 
-	del_fruit = input('Please remove a fruit from the list\n')
-	fruits = 2 * fruits
-	fruits = list(filter(lambda fruit: fruit != del_fruit, fruits))
-	print(fruits)
+    del_fruit = input('Please remove a fruit from the list\n')
+    fruits = 2 * fruits
+    fruits = list(filter(lambda fruit: fruit != del_fruit, fruits))
+    print(fruits)
 
-	for idx, fruit in enumerate(fruits):
-		
-		bool = True
-		while bool:
-			like_fruit = input('Do you like {}?\n'.format(fruit.lower()))
-			if like_fruit == 'yes' or like_fruit == 'no':
-				bool = False
-				if like_fruit == 'no':
-					del fruits[idx]
-					idx -= 1
-			else:
-				print('Please type yes or no')
+    new_fruits = list()
+    for fruit in fruits:
+        bool = True
+        while bool:
+            like_fruit = input('Do you like {}?\n'.format(fruit.lower()))
+            if like_fruit == 'yes' or like_fruit == 'no':
+                bool = False
+                if like_fruit == 'yes':
+                    new_fruits.append(fruit)
+            else:
+                print('Please type yes or no')
 
-	print(fruits)
+    fruits = new_fruits
+    del new_fruits
+    print(fruits)
 
 
 if __name__ == '__main__':
-	fruit_lst()
+    fruit_lst()
+    print(rev_str('test'))
