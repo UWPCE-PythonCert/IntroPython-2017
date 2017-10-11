70f19fd7df342f033490fda4c5f10356e45f894c
chops77 <s.chao@f5.com>
2017-10-11 16:57:38 -0700

    Session 2 exercises

--- students/Chao/session02/fizzbuzz.py
+++ students/Chao/session02/fizzbuzz.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python
+
 def fibu():
     for i in range(100):
         x = i+1
-        if x%3 == 0 and x%5==0:
+        if x%15 == 0:
             print('FizzBuzz')
         elif x%3 == 0:
             print('Fizz')
         elif x%5 == 0:
             print('Buzz')
-        print(x)
+        else:
+            print(x)
 
-fibu()
+# Test time
+if __name__ == "__main__":
+    fibu()
--- students/Chao/session02/print_grid.py
+++ students/Chao/session02/print_grid.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-"""
-Print a grid
-"""
-
+# A fixed size grid
 def print_grid():
     
     for i in range(11):
@@ -12,15 +9,23 @@ def print_grid():
         else:
             print("|         |         |")
 
-
+# 2x2 grid with a size variable
 def print_grid2(n):
+    for i in range(2):
+        print('+' + n*' -' + ' +' + n* ' -' + ' +')
+        for i in range(n):
+            print('|' + n*'  ' + ' |' + n* '  ' + ' |')
+    print('+' + n*' -' + ' +' + n* ' -' + ' +')
 
-    print ('+', n*' - ', '+', n* ' - ', '+')
-    for i in range(n):
-        if n//2 == 0:
-            print ('+', n*' - ', '+', n* ' - ', '+')
-        else:
-            print ('|', n*'   ', '|', n*'   ', '|')
-    print ('+', n*' - ', '+', n* ' - ', '+')
+# 2 size variables now
+def print_grid3(n, m):
+    row = (('+ ' + m*'- ')*n + '+' + '\n')
+    column = ((('| ' + m*'  ')*n + '|' + '\n')*m)
+    block = row+column
+    print(block*n + row)
 
-print_grid2(10)
+# Some tests
+if __name__ == "__main__":
+    print_grid()
+    print_grid2(6)
+    print_grid3(3, 5)
