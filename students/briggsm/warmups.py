a88316332c7207edb369b496be9afce12dfadfd9
Matt Briggs <mabrigg@microsoft.com>
2017-10-16 20:02:45 -0700

    adding current sketches for Matt Briggs

---
+++ students/briggsm/warmups.py
@@ -0,0 +1,30 @@
+def sleep_in(weekday, vacation):
+    '''Warmup-1 > sleep_in '''
+    if vacation == True:
+        return True
+    else:
+        if weekday == False:
+            return True
+        else:
+            return False
+
+def monkey_trouble(a_smile, b_smile):
+    '''Warmup-1 > monkey_trouble'''
+    if a_smile == b_smile:
+        return True
+    else:
+        return False
+
+def sum_double(a, b):
+    '''Warmup-1 > sum_double'''
+    if a == b:
+        return (a+b)*2
+    else:
+        return a + b
+
+def diff21(n):
+    '''Warmup-1 > diff21'''
+      
+
+print (monkey_trouble(True, False))
+  
