39e38ef3869355a76a0247763ec0cad982e80392
alinafe82 <alinafe.matenda@gmail.com>
2017-10-17 09:34:35 -0700

    Adding more work.

---
+++ students/alinafe/session03/firstLastFour.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#with the first and last 4 items removed, and every other item in between
+def first_last_four_removed(seq):
+    if isinstance(seq, tuple):
+        seq = list(seq)
+        del seq[-4:]
+        seq.pop(0)
+        return tuple(seq)
+    elif isinstance(seq, list):
+        del seq[-4:]
+        seq.pop(0)
+        return seq
+    elif isinstance(seq, str):
+        seq=seq[1:-4]
+        return seq
+
+if __name__ == "__main__":
+    # some tests
+    a_string = "this is a string"
+    a_tuple = (2, 54, 13, 12, 5, 32)
+    a_list = [2, 54, 13, 12, 5, 32]
+    assert first_last_four_removed(a_string) == "his is a st"
+    assert first_last_four_removed(a_tuple) == (54,)
+    assert first_last_four_removed(a_list) == [54]
+    print("All tests passed")
+
