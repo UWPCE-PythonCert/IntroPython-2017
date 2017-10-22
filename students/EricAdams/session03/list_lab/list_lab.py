7a2fc2f5988bed50d30b6aa24181e21a752381d7
Eric <theadamsfamily11@msn.com>
2017-10-22 11:25:38 -0700

    Final commit, ready for review

--- students/EricAdams/session03/list_lab/list_lab.py
+++ students/EricAdams/session03/list_lab/list_lab.py
@@ -25,24 +25,23 @@ fruits_orig = fruits[:]
 # Display the list (plain old print() is fine…).
 print(fruits)
 # Ask the user for another fruit and add it to the end of the list.
-answer = input("> Input a fruit to add to this list")
+answer = input("Input a fruit to add to this list> ")
 fruits.append(answer.strip().title())
 # Display the list.
 display_list(fruits)
-display_list(fruits_orig)
 # Ask the user for a number and display the number back to the user and the
 # fruit corresponding to that number (on a 1-is-first basis). Remember that
 # Python uses zero-based indexing, so you will need to correct.
-number = int(input(" Enter a number from 1 to " + str(len(fruits))))
+number = int(input(" Enter a number from 1 to " + str(len(fruits)) + "> "))
 print_what_was_entered(number, fruits)
 # Add another fruit to the beginning of the list using “+” and display the
 # list.
-answer = input("> enter a fruit to add to the beginning of the list")
+answer = input("enter a fruit to add to the beginning of the list> ")
 fruits = [answer.strip().title()] + fruits
 display_list(fruits)
 # Add another fruit to the beginning of the list using insert() and display
 # the list.
-answer = input("> enter a fruit to add to the beginning of the list")
+answer = input("> enter a fruit to add to the beginning of the list> ")
 fruits.insert(0, answer.strip().title())
 display_list(fruits)
 # Display all the fruits that begin with “P”, using a for loop.
@@ -58,11 +57,12 @@ fruits = fruits_orig[:]
 # Display the list.
 display_list(fruits)
 # Remove the last fruit from the list.
+print("Removing the last fruit from the list")
 fruits.remove(fruits[-1])
 # Display the list.
 display_list(fruits)
 # Ask the user for a fruit to delete and find it and delete it.
-answer = input("Enter the fruit you want to be deleted.")
+answer = input("Enter the fruit you want to be deleted.> ")
 answer = answer.strip().title()
 print("You chose {}".format(answer))
 i = 0
@@ -73,6 +73,7 @@ for x in fruits:
 display_list(fruits)
 # (Bonus: Multiply the list times two. Keep asking until a match is found.
 # Once found, delete all occurrences.)
+print("Multipying the list by two")
 fruits = fruits * 2
 display_list(fruits)
 statement = "Enter the fruit you want to be deleted."
@@ -96,19 +97,53 @@ display_list(fruits)
 
 # Series 3
 print("\n Series 3\n")
-display_list(fruits)
 # Again, using the list from series 1:
 fruits = fruits_orig[:]
+display_list(fruits)
 # Ask the user for input displaying a line like “Do you like apples?” for each
 # fruit in the list (making the fruit all lowercase).
-# For each “no”, delete that fruit from the list.
-# For any answer that is not “yes” or “no”, prompt the user to answer with one
-# of those two values (a while loop is good here)
+i = 0
+for x in fruits:
+    answer = input('Do you like {}> '.format(x.lower()))
+    # For each “no”, delete that fruit from the list.
+    if answer.strip().lower() == 'no':
+        print('Deleting {} from the list'.format(x.lower()))
+        counter = 0
+        for y in fruits:
+            if fruits[counter] == x:
+                fruits.remove(x)
+    elif answer.strip().lower() == 'yes':
+        continue
+    # For any answer that is not “yes” or “no”, prompt the user to answer
+    # with one of those two values (a while loop is good here)
+    else:
+        while True:
+            answer = input('Please enter a "yes" or a "no"> ')
+            if answer.strip().lower() == 'yes':
+                break
+            if answer.strip().lower() == 'no':
+                break
 # Display the list.
-
+display_list(fruits)
 # Series 4
+print("\n Series 4\n")
 # Once more, using the list from series 1:
-
+print('List from Series 1:')
+fruits = fruits_orig[:]
+display_list(fruits)
 # Make a copy of the list and reverse the letters in each fruit in the copy.
+copy_fruits = fruits[:]
+print('Reversing the letters in each fruit, (copy_fruits')
+print("copy_fruits before reversing")
+print(copy_fruits)
+i = 0
+for x in copy_fruits:
+    x = x[:: - 1]
+    copy_fruits[i] = x
+    i += 1
+print("copy_fruits after reversing\n", copy_fruits)
 # Delete the last item of the original list. Display the original list and
 # the copy.
+print("deleting the last item of fruits")
+del fruits[-1]
+print("Original list = {}\nCopy = {}".format(fruits, copy_fruits))
