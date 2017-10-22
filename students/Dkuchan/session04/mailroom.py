7859f92cc85d2dc4254120032b60f10206d05a0a
Dan Kuchan <kuchandw@gmail.com>
2017-10-22 13:51:42 -0700

    added main dataset, added function def outline

--- students/Dkuchan/session04/mailroom.py
+++ students/Dkuchan/session04/mailroom.py
@@ -1,1 +1,38 @@
 #mailroom.py
+#root data--------------------------------------
+roster=[['Dan',100,200,300],['Abbey',100,200,300],["Megan",100,200,300]]        #roster is a matrix containing names in column 0 and donations in 1,2,or 3
+
+
+
+#root data--------------------------------------
+
+#function definition section--------------------
+
+def thankyou():
+    print(roster[1][3])
+
+
+def createreport():
+
+def quitter():
+
+
+
+
+
+
+
+
+#function definition section--------------------
+
+thankyou()
+
+
+
+
+
+
+
+while true:     #primary function calling loop
+
+
