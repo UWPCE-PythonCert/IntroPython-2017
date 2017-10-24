5bc4577c870052aaba06d84d4782620095a74e3f
Dan Kuchan <kuchandw@gmail.com>
2017-10-24 14:40:01 -0700

    updated scripts

--- students/Dkuchan/session04/testscripts.py
+++ students/Dkuchan/session04/testscripts.py
@@ -1,1 +1,15 @@
 #testscripts.py
+roster=[['Dan',100,200,300],['Abbey',100,200,300],["Megan",100,200,300],['Spencer',100,200,300],['Marylee',100,200,300]]        #roster is a matrix containing names in column 0 and donations in 1,2,or 3
+
+print(roster[2])
+"""
+i=0
+for i in range(0,len(roster)):
+    j=0
+    numgifts=len(roster[i])-1
+    totaldonations=0
+    for j in range(1,numgifts):
+        totaldonations+=roster(i,j)
+    averagedonations=averagedonations=totaldonations/numgifts
+    print(numgifts)
+    """
