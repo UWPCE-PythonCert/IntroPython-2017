90e5bc863310a5615412b76c242a467620fd5ae9
Eric <theadamsfamily11@msn.com>
2017-10-24 08:05:12 -0700

    first verstion of string_formatting_lab was a bust

--- students/EricAdams/session03/string_formatting_lab.py
+++
@@ -1,9 +0,0 @@
-# Write a format string that will take the tuple:
-# ( 2, 123.4567, 10000, 12345.67)
-# and produce:
-# 'file_002 :   123.46, 1.00e+04, 1.23e+04'
-
-def tuple_format(t):
-    print('file_{:03d}:    {:02f}'.format(t[0],t[1]))
-
-tuple_format(( 2, 123.4567, 10000, 12345.67))
