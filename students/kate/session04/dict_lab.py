13bfa858b114a06efadfac7cd12d7d2878a8fd65
Kmarguerite <kmarguerite@mac.com>
2017-10-24 20:39:13 -0700

    finessed text parsing

--- students/kate/session04/dict_lab.py
+++ students/kate/session04/dict_lab.py
@@ -3,5 +3,13 @@
 """
 dictionary lab
 
-
 """
+
+dict = {'name':'Chris', 'city':'Seattle', 'cake':'chocolate'}
+print(dict)
+
+del dict['cake']
+print(dict)
+
+dict.update({'fruit', 'mango'})
+print(dict)
