326ff7b1a31c3176fed28d38c9c6cf1cfd20c134
Kmarguerite <kmarguerite@mac.com>
2017-10-24 20:16:12 -0700

    create filelab.py

---
+++ students/kate/session04/filelab.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+path lab, parse students.txt
+"""
+file = "kmarguerite/MontyPython/IntroPython-2017/examples/Session01/students.txt"
+file = "students.txt"
+
+with open(file) as students:
+    students.readline()
+    students.
+
+#utfa
+
+file.open
