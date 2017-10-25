d80e37b7c28a7056f56cccf8520a027898d17e52
Chris Barker <PythonCHB@gmail.com>
2017-10-24 20:42:23 -0700

    added get_langs.py

--- examples/Session04/recursive_mainloop.py
+++ examples/Session04/recursive_mainloop.py
@@ -15,15 +15,26 @@ def mainloop():
         if ans == "a":
             print("you typed a")
         elif ans == "b":
-            print("you typed b")
+            second_loop()
         elif ans[0] == "q":
             print("quitting")
             # break
             sys.exit()  # what if I use the break, rather than the exit()?
         elif ans[0] == "r":  # here to test recursion...
             raise Exception()
-        else:  # no expected response -- start the loop again
-            mainloop()
+        # else:  # no expected response -- start the loop again
+        #     mainloop()
+
+def second_loop():
+    while True:
+        ans = input('type "a", "b", or "go back')
+        if ans == "a":
+            print("you typed a")
+        elif ans == "b":
+            second_loop()
+        elif ans[0] == "g":
+            return
+
 
 
 if __name__ == "__main__":
