d26d985de90773118dd9489e36d73952961cbdcd
Dan Kuchan <kuchandw@gmail.com>
2017-10-29 15:44:50 -0700

    Created dictlab with basic code

---
+++ students/Dkuchan/session04/dictlab.py
@@ -0,0 +1,23 @@
+#dictlab.py
+#'name' : 'Chris' , 'city' : 'Seattle' , 'cake' : 'Chocolate'
+
+
+#CREATE DICT
+dandict = dict()
+dandict['name'] = 'Chris'
+dandict['city'] = 'Seattle'
+dandict['cake'] = 'Chocolate'
+
+#DISPLAY DICT
+print(dandict)
+
+print()
+print()
+
+
+#DELETE ENTRY FOR CAKE
+del dandict['cake']
+
+#DISPLAY DICT
+print(dandict)
+
