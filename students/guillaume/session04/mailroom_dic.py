7695c01cbe06b7469f7cc3ebc9dfbfce60b58ab1
guillaumethomas <guillaumethomas@outlook.com>
2017-10-29 22:27:52 -0700

    Sunday Night

--- students/guillaume/session04/mailroom_dic.py
+++ students/guillaume/session04/mailroom_dic.py
@@ -2,24 +2,6 @@
 from os import system
 
 
-def read_data(textfile):
-    '''
-    Read data from a textfile about the donor and the
-    amount of donations
-    Pattern is as follow:
-    '''
-    file = open(textfile, 'r')
-    donors = list()
-    for i in list(file.readlines()):
-        donors.append((i.rstrip()).split(','))
-        for i, s in enumerate(donors[-1]):
-            donors[-1][i] = s.lstrip()
-            if donors[-1][i].isdecimal():
-                donors[-1][i] = int(s)
-    file.close()
-    return donors
-
-
 def read_data_dic(textfile):
     '''
     Read data from a textfile about the donor and the
@@ -29,21 +11,26 @@ def read_data_dic(textfile):
     file = open(textfile, 'r')
     donors = list()
     for i in list(file.readlines()):
-        donors.append((i.rstrip()).split(','))
-        for i, s in enumerate(donors[-1]):
-            donors[-1][i] = s.lstrip()
-            if donors[-1][i].isdecimal():
-                donors[-1][i] = int(s)
+        tmp_lst = i.rstrip().split(',')
+        tmp_lst[2] = tmp_lst[2].lstrip(' [')
+        tmp_lst[-1] = tmp_lst[-1].rstrip(']')
+        print(tmp_lst)
+        tmp_dic = {'first_name': tmp_lst[0].lstrip(),
+                   'last_name': tmp_lst[1].lstrip(),
+                   'donations': [int(s.lstrip()) for s in tmp_lst[2:]]}
+        donors.append(tmp_dic)
     file.close()
+    print(donors)
     return donors
 
 
-def write_data(textfile, donors):
+def write_data_dic(textfile, donors):
     '''
     '''
     file = open(textfile, 'w')
     for donor in donors:
-        file.write('{}\n'.format(', '.join(map(str, donor))))
+        tmp_str = '{first_name}, {last_name}, {donations}'.format(**donor)
+        file.write('{}\n'.format(tmp_str))
     file.close()
 
 
@@ -57,7 +44,7 @@ def add_donation(ind, donors):
             # need to work on test for float donation
             boolean = False
             donation = int(donation)
-            donors[ind].append(donation)
+            donors[ind][donations].append(donation)
     return donors, donation
 
 
@@ -82,8 +69,8 @@ def email_thank_you(donor, donation, charity='local charity'):
 def send_thank_you(textfile):
     '''
     '''
-    donors = read_data(textfile)
-    names = ['{} {}'.format(i[0], i[1]) for i in donors]
+    donors = read_data_dic(textfile)
+    names = ['{first_name} {last_name}'.format(**donor) for donor in donors]
     boolean = True
     # Menu for Name cases
     while boolean:
@@ -104,7 +91,7 @@ def send_thank_you(textfile):
         else:
             print('Make sure you enter Last & first name\n')
 
-    write_data(textfile, donors)
+    write_data_dic(textfile, donors)
     email = email_thank_you(name, donation)
     return email
 
@@ -120,7 +107,8 @@ def quit(textfile):
 
 
 def report(textfile):
-    donors = read_data(textfile)
+    print('report')
+    donors = read_data_dic(textfile)
     str_donors = [['{} {}'.format(i[0], i[1])] for i in donors]
     for ind, donor in enumerate(donors):
         add_list = [sum(donor[2:]), len(donor[2:])]
@@ -136,7 +124,8 @@ Donor Name                | Total Given | Num Gifts | Average Gift
         table += '{} $  {}  {} $  {}\n'.format(
             donor[0].ljust(27), str(donor[1]).ljust(13),
             str(donor[2]).ljust(8), str(donor[3]).ljust(10))
-    return table
+    print(table)
+    return False
 
 
 def input_sc(textfile):
@@ -164,30 +153,18 @@ def input_sc(textfile):
         selection = input('Please type a number between 1 & 4:\n')
         if selection.isdecimal():
             selection = int(selection)
+            print(type(selection))
+            prog_dic.get(selection, 'None')(textfile)
+
+        '''
         if selection in prog_dic.keys():
-            bool = prog_dic[textfile]
+            bool = prog_dic[selection](textfile)
+            print(type(prog_dic[selection]))
         else:
             system('clear')
             print('Please input a correct entry')
+        '''
 
-
-    '''
-    bool = True
-    while bool:
-        print(welcome)
-        selection = input('Please type a number between 1 & 3:\n')
-        if selection.isdecimal():
-            selection = int(selection)
-            if selection > 0 and selection < 4:
-                if selection == 1:
-                    print(send_thank_you(textfile))
-                elif selection == 2:
-                    print(report(textfile))
-                elif selection == 3:
-                    bool = False
-                    print('Leaving program')
-
-    '''
 if __name__ == '__main__':
     '''
     '''
