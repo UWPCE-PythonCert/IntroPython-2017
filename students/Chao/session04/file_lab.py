efefb161a88d4deed5e04a660afc29f1695ed6e7
chops77 <s.chao@f5.com>
2017-10-29 17:53:34 -0700

    Working on session 4 codes

--- students/Chao/session04/file_lab.py
+++ students/Chao/session04/file_lab.py
@@ -1,6 +1,39 @@
 #!/usr/bin/env python
 
-infile = "students.txt"
+import os
 
-with open(infile) as students:
-    students.readlines()
+def pdir():
+    """ Write a program which prints the full path to all files in the current directory, one per line """
+   
+    for f in os.listdir('.'):
+        # Check if item is a file (not a directory)
+        if os.path.isfile(f):
+            print(f)
+
+def copyf(inputf, outputf):
+    """ Write a program which copies a file from a source, to a destination """
+
+    with open(inputf,'rb') as f_in, open(outputf,'wb') as f_out:
+        while True:
+            # Read to buffer, 1 byte at a time
+            buf = f_in.read(1)
+            if buf:
+                f_out.write(buf)
+            else:
+                break
+
+#def langlist():
+
+#    with open(inputf,'r') as slist:
+
+
+# infile = "students.txt"
+
+# with open(infile) as students:
+#     students.readlines()
+
+
+if __name__ == '__main__':
+    pdir()
+    copyf('students.txt', 'students_backup.txt')
+    copyf('spongebob.jpg', 'spongebob_backup.jpg')
