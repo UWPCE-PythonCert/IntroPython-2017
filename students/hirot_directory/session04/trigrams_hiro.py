508abedb2a3d705d89f264b4e3b25fb142f3ebd2
Hiroyuki Takechi <hiroyukitakechi@gmail.com>
2017-10-29 22:22:00 -0700

    adding .setdefault approach

--- students/hirot_directory/session04/trigrams_hiro.py
+++ students/hirot_directory/session04/trigrams_hiro.py
@@ -1,36 +1,51 @@
 words = "I wish may I wish I might".split()
-#print(words)
 
+print("The approach from class")
 
-trigrams = {} #empty dict
+trigrams = {}
 
 for i in range(len(words)-2):
-	pair = tuple(words[i:i+2])
-	follower = words[i+2]
-	print(pair, follower)
-	if pair in trigrams:
-		trigrams[pair].append(follower)
-	else:
-		trigrams[pair] = [follower] #key:pair
+    pair = tuple(words[i:i+2])
+    follower = words[i+2]
+    print(pair, follower)
+    if pair in trigrams:
+        trigrams[pair].append(follower)
+    else:
+        trigrams[pair] = [follower] #key:pair
 
 print(trigrams)
 
-#this can be called by one dict method. Find the method.
+# .item :
 
-#print dict.keys()
-#print dict.values()
-#print dict.items()
+print("The approach with .item()")
 
-# .item() shows the same result. 
+words = "I wish may I wish I might".split()
 
-trigrams = {} #empty dict
+trigrams = {}
 
 for i in range(len(words)-2):
-	pair = tuple(words[i:i+2])
-	follower = words[i+2]
-	print(pair, follower)
-	trigrams[pair] = follower
+    pair = tuple(words[i:i+2])
+    follower = words[i+2]
+    print(pair, follower)
+    trigrams[pair] = follower
 
-#print(trigrams.keys())
-#print(trigrams.values())
 print(trigrams.items())
+
+
+# .setdefault :
+
+print("THe approach with .setdefault")
+
+words = "I wish may I wish I might".split()
+
+trigrams = {}
+
+for i in range(len(words)-2):
+    pair = tuple(words[i:i+2])
+    follower = words[i+2]
+    print(pair, follower)
+    if pair not in trigrams:
+        trigrams.setdefault(pair, follower)        
+    # else:
+    #     trigrams[pair].append(follower)        
+print(trigrams)
