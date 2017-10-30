f11d5519a5215b30a536dbb9412125ad2e44743b
guillaumethomas <guillaumethomas@outlook.com>
2017-10-29 23:01:38 -0700

    Sunday Night

--- students/guillaume/session04/mailroom_dic.py
+++ students/guillaume/session04/mailroom_dic.py
@@ -14,13 +14,11 @@ def read_data_dic(textfile):
         tmp_lst = i.rstrip().split(',')
         tmp_lst[2] = tmp_lst[2].lstrip(' [')
         tmp_lst[-1] = tmp_lst[-1].rstrip(']')
-        print(tmp_lst)
         tmp_dic = {'first_name': tmp_lst[0].lstrip(),
                    'last_name': tmp_lst[1].lstrip(),
                    'donations': [int(s.lstrip()) for s in tmp_lst[2:]]}
         donors.append(tmp_dic)
     file.close()
-    print(donors)
     return donors
 
 
@@ -44,7 +42,12 @@ def add_donation(ind, donors):
             # need to work on test for float donation
             boolean = False
             donation = int(donation)
-            donors[ind][donations].append(donation)
+            print(donors[ind])
+            print(type(donors[ind]))
+            if 'donation' not in donors[ind].keys():
+                donors[ind]['donations'] = [donation]
+            else:
+                donors[ind]['donations'].append(donation)
     return donors, donation
 
 
@@ -54,9 +57,9 @@ def email_thank_you(donor, donation, charity='local charity'):
     email = '''
             Dear {},
 
-            I would like to thank you personnaly for the generous
+            I would like to thank you persnnally for the generous
             donation of ${} that you have made recently.
-            Your committment to our cause is very valuable to us.
+            Your commmitment to our cause is very valuable to us.
 
             Best regards,
 
@@ -70,6 +73,7 @@ def send_thank_you(textfile):
     '''
     '''
     donors = read_data_dic(textfile)
+    print(donors)
     names = ['{first_name} {last_name}'.format(**donor) for donor in donors]
     boolean = True
     # Menu for Name cases
@@ -86,18 +90,36 @@ def send_thank_you(textfile):
             # Checking for Last & First Name - very primitive
             boolean = False
             names.append(name)
-            donors.append(name.split(' '))
+            donor = name.split(' ')
+            print(donor)
+            donors.append({'first_name': donor[0], 'last_name': donor[1]})
             donors, donation = add_donation(-1, donors)
         else:
             print('Make sure you enter Last & first name\n')
 
     write_data_dic(textfile, donors)
     email = email_thank_you(name, donation)
-    return email
+    print(email)
+    return True
 
 
 def mass_mailing(textfile):
-    pass
+    donors = read_data_dic(textfile)
+    for donor in donors:
+        file = open('{first_name}_{last_name}_thank_you.txt'.format(**donor), 'w')
+        letter = '''
+            Dear {first_name} {last_name},
+
+            I would like to thank you personally for your generous
+            donations that you have made recently.
+            Your commitment to our cause is very valuable to us.
+
+            Best regards,
+
+                                    John Adams
+            '''.format(**donor)
+        file.write(letter)
+        file.close
     return True
 
 
@@ -109,9 +131,10 @@ def quit(textfile):
 def report(textfile):
     print('report')
     donors = read_data_dic(textfile)
-    str_donors = [['{} {}'.format(i[0], i[1])] for i in donors]
+    str_donors = [['{first_name} {last_name}'.format(**donor)]
+                  for donor in donors]
     for ind, donor in enumerate(donors):
-        add_list = [sum(donor[2:]), len(donor[2:])]
+        add_list = [sum(donor['donations']), len(donor['donations'])]
         add_list.append(round(add_list[0] / add_list[1], 2))
         str_donors[ind].extend(add_list)
     str_donors.sort(key=lambda x: x[1])
@@ -125,7 +148,7 @@ Donor Name                | Total Given | Num Gifts | Average Gift
             donor[0].ljust(27), str(donor[1]).ljust(13),
             str(donor[2]).ljust(8), str(donor[3]).ljust(10))
     print(table)
-    return False
+    return True
 
 
 def input_sc(textfile):
@@ -137,7 +160,7 @@ def input_sc(textfile):
                      'Quit': '4 - Quit'}
 
     welcome = """\nlocal charity donor database\n
-    Please make a choice:
+    Please make a choice:\n
     {Thank_you} \n
     {Report} \n
     {Mass_Mailing} \n
@@ -153,17 +176,8 @@ def input_sc(textfile):
         selection = input('Please type a number between 1 & 4:\n')
         if selection.isdecimal():
             selection = int(selection)
-            print(type(selection))
-            prog_dic.get(selection, 'None')(textfile)
+            bool = prog_dic.get(selection, 'None')(textfile)
 
-        '''
-        if selection in prog_dic.keys():
-            bool = prog_dic[selection](textfile)
-            print(type(prog_dic[selection]))
-        else:
-            system('clear')
-            print('Please input a correct entry')
-        '''
 
 if __name__ == '__main__':
     '''
