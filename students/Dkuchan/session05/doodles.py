0217389c23c0008ba4148021c0a975252b7ca62a
Dan Kuchan <kuchandw@gmail.com>
2017-10-31 16:44:03 -0700

    updated doodles

--- students/Dkuchan/session05/doodles.py
+++ students/Dkuchan/session05/doodles.py
@@ -4,3 +4,21 @@
 r = lambda x, y: x**2 + y**2
 
 print(r(3, 2))
+
+
+xlen=13
+gapfnc=int((xlen-3)/2)
+ylen=11
+yleni=int(ylen)
+half=int(ylen/2)+1
+outputbox="+" + "-" * gapfnc + "+" + "-" * gapfnc + "+"
+noncenter="|" + " " * gapfnc + "|" + " " * gapfnc + "|"
+for i in range(0,ylen+1):
+    if i==0 or i==half or i==yleni:
+        print(outputbox)
+    else:
+        print(noncenter)
+
+
+#danstring="Hello my name is dan"*3 + "what do you think of that?"
+#print(outputbox)
