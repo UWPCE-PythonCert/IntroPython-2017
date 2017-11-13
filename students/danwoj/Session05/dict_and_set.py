af4c240113700c90d80411c804242d773425a545
danwoj <danwoj80@gmail.com>
2017-11-12 17:12:51 -0800

    Updating with the unit testing exercises I did with Cigar Party and two other Codingbat examples.

--- students/danwoj/Session05/dict_and_set.py
+++ students/danwoj/Session05/dict_and_set.py
@@ -9,13 +9,11 @@ food_prefs = {"name": "Chris",
 
 def mainloop():
 
-	print('{name} is from {city}, and he likes, {cake} cake, {fruit} fruit, {salad} salad, and {pasta} pasta'.format(**food_prefs))
+	print('{name} is from {city}, and he likes, {cake} cake, {fruit} fruit, {salad} salad, and {pasta} pasta\n'.format(**food_prefs))
 
-	print(dict([(i, hex(i)) for i in range(16)]))
+	print(dict([(i, hex(i)) for i in range(16)]), '\n')
 
-	print({i: hex(i) for i in range(16)})
-
-	print({key: val.count('a') for key, val in food_prefs.items()})
+	print({key: val.count('a') for key, val in food_prefs.items()}, '\n')
 
 	s2 = {i for i in range(20) if i % 2 == 0}
 
@@ -31,27 +29,15 @@ def mainloop():
 
 	set_list = [set() for i in seq]
 
-	for i, j in zip(seq, set_list):
-		[j.add(k) for k in range(21) if not j % i]
-
-	print(set_list)
-
 	n = 5
 	divisors = range(2, n + 1)
-	print("divisors ", divisors)
-#	# create a list of empty sets
-#	sets = [set() for i in divisors]
-#	print(sets)
+	# create a list of empty sets
+	sets = [set() for i in divisors]
 
-#	for i, st in zip(divisors, sets):
-#		[st.add(j) for j in range(21) if not j % i]
-#
-#	print("\nHere are all the sets:\n", sets)
+	for i, st in zip(divisors, sets):
+		[st.add(j) for j in range(21) if not j % i]
+
+	print("\nHere are all the sets:\n", sets)
 
 if __name__ == '__main__':
 	mainloop()
-
-
-#	Create sets s2, s3 and s4 that contain numbers from zero through twenty, divisible 2, 3 and 4.
-
-#        Do this with one set comprehension for each set.
