6d188fe0c1a5253299e994b649c476f52c69bc11
Chris Barker <PythonCHB@gmail.com>
2017-11-16 22:28:37 -0800

    removed tab indentation...

--- students/Chris/session07/html_render.py
+++ students/Chris/session07/html_render.py
@@ -1,40 +1,42 @@
 
 
 class Element():
-	tag = 'html'
-	indent = '  '
-	def __init__(self, content=None):
-		if content is None:
-			self.content = []
-		else:
-			self.content = [content]
 
-	def append(self, content):
-		self.content.append(content)
+    tag = 'html'
+    indent = '  '
 
-	def render(self, file_obj):
-		all_content = ('<' + self.tag + '>')
-		for each in self.content:
-			try:
-				all_content += each
-			except TypeError:
-				each.render(file_obj)
-		all_content += '</' + self.tag + '>'
-		
-		self.write_to_file(file_obj, all_content)
+    def __init__(self, content=None):
+        if content is None:
+            self.content = []
+        else:
+            self.content = [content]
 
-	def write_to_file(self, file_obj, stuff_to_print):
-		file_obj.write(stuff_to_print)
+    def append(self, content):
+        self.content.append(content)
+
+    def render(self, file_obj):
+        all_content = ('<' + self.tag + '>')
+        for each in self.content:
+            try:
+                all_content += each
+            except TypeError:
+                each.render(file_obj)
+        all_content += '</' + self.tag + '>'
+
+        self.write_to_file(file_obj, all_content)
+
+    def write_to_file(self, file_obj, stuff_to_print):
+        file_obj.write(stuff_to_print)
 
 
 class Body(Element):
-	tag = 'body'
+    tag = 'body'
 
 
 class Para(Element):
-	tag = 'p'
+    tag = 'p'
 
 
 class HTML(Element):
-	tag = 'html'
+    tag = 'html'
 
