cbcb572dfe8bc3b0653b71f8348891af3c59dba0
Eowyn Baughman <eowyn42@gmail.com>
2017-11-19 18:51:42 -0800

    finished and tested steps 1-3 on html render assignment

--- students/eowyn/session07/html-rendering/html_render.py
+++ students/eowyn/session07/html-rendering/html_render.py
@@ -2,7 +2,7 @@
 
 class Element():
     tag = 'html'
-    indent = 4*' '
+    indent = 4 * ' '
 
     def __init__(self, content=None):
         # import pdb; pdb.set_trace()
@@ -14,7 +14,7 @@ class Element():
     def append(self, content):
         self.content.append(content)
 
-    def render(self, out_file, ind = ""):
+    def render(self, out_file, ind=""):
         if self.tag == 'html':
             out_file.write('<!DOCTYPE html>')
         out_file.write('\n')
@@ -27,10 +27,6 @@ class Element():
         if ind != "":
             out_file.write('\n' + ind)
         out_file.write('</' + self.tag + '>')
-
-
-
-
 
     def write_to_file(self, file_obj, stuff_to_print):
         file_obj.write(stuff_to_print)
@@ -48,3 +44,21 @@ class Html(Element):
     tag = 'html'
 
 
+class Head(Element):
+    tag = 'head'
+
+
+class OneLineTag(Element):
+
+    def render(self, out_file, ind=""):
+        out_file.write('\n' + ind + '<' + self.tag + '>')
+        for each in self.content:
+            try:
+                each.render(out_file)
+            except AttributeError:
+                out_file.write(str(each))
+        out_file.write('</' + self.tag + '>')
+
+
+class Title(OneLineTag):
+    tag = 'title'
--- students/eowyn/session07/html-rendering/test_html_render.py
+++ students/eowyn/session07/html-rendering/test_html_render.py
@@ -1,5 +1,5 @@
 import html_render
-from html_render import Element, Body, P, Html
+from html_render import Element, Body, P, Html,Head,OneLineTag,Title
 
 def test_new_element():
     el_obj = Element
@@ -31,7 +31,7 @@ def test_tag_exists():
 
 def test_indent_exists():
     # could alternately test that it is any number of spaces
-    assert Element.indent == 4 * ' '
+    assert ' ' in Element.indent
 
 
 def test_render():
@@ -73,9 +73,33 @@ def test_render_body():
     assert more_stuff in contents
 
 def test_render_non_strings():
-    my_stuff= 'hi'
-    el_object = Element(Body('any string I like'))
-    with open('test1', 'w') as out_file:
+    my_stuff = 'any string I like'
+    el_object = Element(Body(my_stuff))
+    with open('test3', 'w') as out_file:
         el_object.render(out_file)
-    with open('test1', 'r') as in_file:
+    with open('test3', 'r') as in_file:
         contents = in_file.read()
+    assert my_stuff in contents
+
+def test_head_tag():
+    newobj = Head()
+    assert newobj.tag == 'head'
+
+
+def test_title_tag():
+    assert Title().tag == 'title'
+
+
+def test_title_render():
+    my_stuff= 'bye bye ms american pie'
+    titleobj = Title(my_stuff)
+    more_stuff = ' drove my chevy to the levy'
+    titleobj.append(more_stuff)
+    with open('test2', 'w') as out_file:
+        titleobj.render(out_file)
+    with open('test2', 'r') as in_file:
+        contents = in_file.read()
+    assert contents.startswith('\n<title>')
+    assert contents.endswith('</title>')
+    assert my_stuff in contents
+    assert more_stuff in contents
