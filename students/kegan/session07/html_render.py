1c2612023178e9d3258a0d48205f33add2b2fd82
Kathryn Egan <kathryn.s.nichols@gmail.com>
2017-11-24 16:18:25 -0800

    added meta and comments

--- students/kegan/session07/html_render.py
+++ students/kegan/session07/html_render.py
@@ -1,13 +1,20 @@
 """
 Kathryn Egan
+
 """
 
 
 class Element:
+    """ Stores behavior and data for an Element of HTML. """
     indent = '    '
-    tag = 'html'
 
     def __init__(self, content=None, **kwargs):
+        """ Initializes an Element with content, if given
+        and any HTML attributes of the Element.
+        Args:
+            content (str or Element) : text or a nested Element
+            kwargs (dic str:str) : HTML attributes for Element
+        """
         if content is None:
             self.content = []
         else:
@@ -15,92 +22,165 @@ class Element:
         self.attrs = kwargs
 
     def append(self, content):
+        """ Adds content to this Element.
+        Args:
+            content (str or Element) : text or a nested Element
+        """
         self.content.append(content)
 
     def render(self, file_out, ind=0):
-        file_out.write(self.open_tag(ind))
+        """ Renders the contents of this Element into HTML
+        format and writes to file using given file handler.
+        Args:
+            file_out (TextIOWrapper) : file handler
+            ind (str) : level of indentation for this Element
+        """
+        file_out.write(self.get_tag(ind, 'open'))
         file_out.write('\n')
         for c in self.content:
             try:
                 c.render(file_out, ind + 1)
             except AttributeError:
                 file_out.write('{}{}\n'.format((ind + 1) * self.indent, c))
-        file_out.write(self.close_tag(ind))
+        file_out.write(self.get_tag(ind, 'close'))
         file_out.write('\n')
 
-    def open_tag(self, ind, selfclosing=False):
-        attrs = ''.join([
-            ' {}="{}"'.format(key, self.attrs[key])
-            for key in sorted(self.attrs)])
-        close = ' /' if selfclosing else ''
-        tag = '{}<{}{}{}>'.format(ind * self.indent, self.tag, attrs, close)
-        return tag
-
-    def close_tag(self, ind):
-        tag = '{}</{}>'.format(ind * self.indent, self.tag)
+    def get_tag(self, ind, style):
+        """ Generates a tag of according to given style with given indent.
+        Style can be 'open', 'close', or 'selfclosing' (defaults to 'open').
+        Args:
+            ind (str) : level of indent
+            style (str) : style to use
+        Returns:
+            tag : formatted tag
+        """
+        substrings = {
+            'indent': ind * self.indent,
+            'tag': self.tag,
+            'attributes':
+                '' if style == 'close' else ''.join([
+                    ' {}="{}"'.format(key, self.attrs[key])
+                    for key in sorted(self.attrs)]),
+            'close': '/' if style == 'close' else '',
+            'selfclosing': ' /' if style == 'selfclosing' else ''}
+        tag = '{indent}<{close}{tag}{attributes}{selfclosing}>'
+        tag = tag.format(**substrings)
         return tag
 
 
 class OneLineTag(Element):
+    """ Handles ne-line HTML elements. """
+
     def render(self, file_out, ind=0):
-        file_out.write(self.open_tag(ind))
+        """ Renders the contents of this Element into HTML
+        format on one line and writes to file using given file handler.
+        Args:
+            file_out (TextIOWrapper) : file handler
+            ind (str) : level of indentation for this Element
+        """
+        file_out.write(self.get_tag(ind, 'open'))
         for c in self.content:
             file_out.write(c)
-        file_out.write(self.close_tag(0))
+        file_out.write(self.get_tag(0, 'close'))
         file_out.write('\n')
 
 
 class SelfClosingTag(Element):
+    """ Handles contentless HTML elements. """
+
     def render(self, file_out, ind=0):
-        file_out.write(self.open_tag(ind, selfclosing=True))
+        """ Renders this content-less Element into HTML
+        and writes to file using given file handler.
+        Args:
+            file_out (TextIOWrapper) : file handler
+            ind (str) : level of in
+        """
+        file_out.write(self.get_tag(ind, 'selfclosing'))
         file_out.write('\n')
 
 
 class A(OneLineTag):
+    """ HTML hyperlink element. """
     tag = 'a'
 
     def __init__(self, link, content):
+        """ Maps link to content as an HTML attribute of an Element.
+        Args:
+            link (str) : hyperlink
+            content (str) : content to map hyperlink to
+        """
         Element.__init__(self, content, href=link)
 
 
 class Title(OneLineTag):
+    """ HTML title element. """
     tag = 'title'
 
 
 class Hr(SelfClosingTag):
+    """ HTML horizontal line element. """
     tag = 'hr'
 
 
 class Br(SelfClosingTag):
+    """ HTML line break element. """
     tag = 'br'
 
 
+class Meta(SelfClosingTag):
+    """ HTML head encoding. """
+    tag = 'meta'
+
+
 class Head(Element):
+    """ HTML Head element. """
     tag = 'head'
 
 
 class Html(Element):
+    """ HTML page. """
     tag = 'html'
+
+    def render(self, file_out, ind=0):
+        """ Renders the contents of this Element into HTML
+        format headed by doctype. Writes to file using given file handler.
+        Args:
+            file_out (TextIOWrapper) : file handler
+            ind (str) : level of indentation for this Element
+        """
+        file_out.write('<!DOCTYPE {}>\n'.format(self.tag))
+        Element.render(self, file_out, ind=1)
 
 
 class Body(Element):
+    """ HTML body element. """
     tag = 'body'
 
 
 class P(Element):
+    """ HTML paragpraph element. """
     tag = 'p'
 
 
 class Ul(Element):
+    """ HTML unordered list. """
     tag = 'ul'
 
 
 class Li(Element):
+    """ HTML list element. """
     tag = 'li'
 
 
-class Header(OneLineTag):
+class H(OneLineTag):
+    """ HTML header element. """
 
     def __init__(self, level, content):
+        """ Initializes this element with the given level and content.
+        Args:
+            level (int) : level of this header
+            content (str) : content of this header
+        """
         self.tag = 'h' + str(level)
         Element.__init__(self, content)
+
