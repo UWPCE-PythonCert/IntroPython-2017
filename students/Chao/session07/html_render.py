a4295e29cb6f4b5cbc1b678bf367e2dba4baa8ac
chops77 <s.chao@f5.com>
2017-11-27 16:12:37 -0800

    Html render exercise updated to step 3

--- students/Chao/session07/html_render.py
+++ students/Chao/session07/html_render.py
@@ -2,7 +2,7 @@
 
 class Element():
     tag = 'html'
-    indent = ""
+    indent = ''
 
     def __init__(self, content=None):
         if content is None:
@@ -12,18 +12,16 @@ class Element():
 
     def append(self, content):
         self.content.append(content)
-        #print(self.content)
 
-    def render(self, file_obj, ind = ""):
-        file_obj.write('<' + self.tag + '>\n')
+    def render(self, file_obj, ind = ''):
+        file_obj.write(ind + '<' + self.tag + '>\n')
         for each in self.content:
+            newind = ind + self.indent
             if type(each) == str:
-                file_obj.write(ind + each + '\n')
+                file_obj.write(newind + each + '\n')
             else:
-                self.content = self.content.content
-                # for a in each.content:
-                #     file_obj.write(ind + a + '\n')
-        file_obj.write('</' + self.tag + '>\n')
+                each.render(file_obj, newind)
+        file_obj.write(ind + '</' + self.tag + '>\n')
 
 class Body(Element):
     tag = 'body'
@@ -33,3 +31,32 @@ class P(Element):
 
 class Html(Element):
     tag = 'html'
+
+    # 'special' reder method for html only - set DOCTYPE, indent, and no indent before tags
+    def render(self, file_obj, ind = ''):
+        Element.indent = ind
+        file_obj.write('<!DOCTYPE html>\n' + '<' + self.tag + '>\n')
+        for each in self.content:
+            if type(each) == str:
+                file_obj.write(ind + each + '\n')
+            else:
+                each.render(file_obj, ind)
+        file_obj.write('</' + self.tag + '>\n')
+
+class Head(Element):
+    tag = 'head'
+
+class OneLineTag(Element):
+
+    def render(self, file_obj, ind = ''):
+        file_obj.write(ind + '<' + self.tag + '>')
+        for each in self.content:
+            newind = ind + self.indent
+            if type(each) == str:
+                file_obj.write(each)
+            else:
+                each.render(file_obj, newind)
+        file_obj.write('</' + self.tag + '>\n')
+
+class Title(OneLineTag):
+    tag = 'title'
