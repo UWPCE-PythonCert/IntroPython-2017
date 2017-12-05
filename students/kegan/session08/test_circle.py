a37c3eb65b4cdb74b42d189c81adf991670db272
Kathryn Egan <kathryn.s.nichols@gmail.com>
2017-12-04 16:45:10 -0800

    cleaned up math operations and added appropriate error handling for radius <= 0

--- students/kegan/session08/test_circle.py
+++ students/kegan/session08/test_circle.py
@@ -3,23 +3,28 @@ Kathryn Egan
 """
 from math import pi
 from Circle import Circle
+import pytest
 
 
 def test_radius_property():
     c = Circle(5)
     assert c.radius == 5
+    with pytest.raises(ValueError):
+        c.radius = 0
+    with pytest.raises(ValueError):
+        c.radius = -2
 
 
 def test_diameter_property():
     c = Circle(2)
-    assert c.diameter == 4
+    assert c.diameter == 2 * 2
 
 
 def test_diameter_setter():
     c = Circle(3)
     c.diameter = 10
     assert c.diameter == 10
-    assert c.radius == 5
+    assert c.radius == 10 / 2
 
 
 def test_area_property():
@@ -29,73 +34,96 @@ def test_area_property():
 
 def test_str():
     c = Circle(1)
-    assert str(c) == 'Circle, r=1'
+    assert str(c) == 'Circle, r=1.0'
+    c.radius = 3.5
+    assert str(c) == 'Circle, r=3.5'
+    c.radius = 5.69793829485950684
+    assert str(c) == 'Circle, r=5.7'
 
 
 def test_repr():
     c = Circle(5)
-    assert repr(c) == 'Circle(5)'
+    assert repr(c) == 'Circle(5.0)'
+    c.radius = 99.929292
+    assert repr(c) == 'Circle(99.9)'
 
 
 def test_add1():
     c1 = Circle(2)
     c2 = Circle(3)
     c3 = c1 + c2
-    assert c3.radius == 5
+    assert c3.radius == 2 + 3
 
 
 def test_add2():
     c1 = Circle(2)
     c2 = c1 + 3
-    assert c2.radius == 5
+    assert c2.radius == 2 + 3
 
 
 def test_add3():
     c1 = Circle(2)
     c2 = 3 + c1
-    assert c2.radius == 5
+    assert c2.radius == 2 + 3
+
+
+def test_add4():
+    c1 = Circle(2)
+    c1 += 4
+    assert c1.radius == 2 + 4
 
 
 def test_multiply1():
     c1 = Circle(3)
     c2 = c1 * 4
-    assert c2.radius == 12
+    assert c2.radius == 3 * 4
+    with pytest.raises(ValueError):
+        c1 * -1
+    with pytest.raises(ValueError):
+        c1 * 0
 
 
 def test_multiply2():
     c1 = Circle(3)
     c2 = 4 * c1
-    assert c2.radius == 12
+    assert c2.radius == 3 * 4
 
 
 def test_multiply3():
     c1 = Circle(3)
     c2 = Circle(4)
     c3 = c1 * c2
-    assert c3.radius == 12
+    assert c3.radius == 3 * 4
+
+
+def test_multiply4():
+    c1 = Circle(3)
+    c1 *= 3
+    assert c1.radius == 3 * 3
 
 
 def test_operators():
     assert Circle(3) < Circle(5)
+    assert not Circle(3) < Circle(2)
+    assert Circle(3) <= Circle(5)
+    assert Circle(3) <= Circle(3)
+    assert not Circle(4) <= Circle(3)
     assert Circle(4) == Circle(4)
+    assert Circle(3) != Circle(5)
+    assert Circle(4) > Circle(3)
+    assert not Circle(4) > Circle(5)
     assert Circle(2) >= Circle(2)
     assert Circle(2) >= Circle(1)
-    assert Circle(3) != Circle(5)
-
-
-def test_sort():
-    c1 = Circle(1)
-    c2 = Circle(2)
-    c3 = Circle(3)
-    c4 = Circle(4)
-    l1 = [c4, c2, c1, c3]
-    l1.sort()
-    assert l1 == [c1, c2, c3, c4]
+    assert not Circle(2) >= Circle(3)
 
 
 def test_reflected():
-    c = Circle(3)
-    assert c * 5 == 5 * c
+    c1 = Circle(3)
+    c1 = c1 * 3
+    assert c1.radius == 3 * 3
+    c2 = Circle(4)
+    c2 = 4 * c2
+    assert c2.radius == 4 * 4
 
 
 def test_division1():
@@ -117,8 +145,8 @@ def test_subtract():
     assert c3.radius == 2
     c1 -= 4
     assert c1.radius == 6
-    c4 = 6 - c2
-    assert c4.radius == -2
+    with pytest.raises(ValueError):
+        6 - c2
 
 
 def test_augmented():
