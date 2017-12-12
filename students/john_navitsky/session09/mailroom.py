172d0350fd9a6b59535803ce468f958fab029176
John Navitsky <johnn@cray.com>
2017-12-11 19:57:36 -0800

    All major functionality working.

--- students/john_navitsky/session09/mailroom.py
+++ students/john_navitsky/session09/mailroom.py
@@ -6,6 +6,8 @@ import datetime
 import hashlib
 from pprint import pprint
 import uuid
+import os
+import pickle
 
 """ Program to manage donations. """
 
@@ -226,12 +228,6 @@ class Donor:
         """ allow bulk setting of donation entries """
         self._donations = value
 
-    # @property
-    # def add_donation(self):
-    #     """ reading not allowed via add_donation """
-    #     raise AttributeError("Read donations with the 'donations' attribute, instead.")
-
-    #@add_donation.setter
     def add_donation(self, value):
         """ add a single donation """
         today = datetime.datetime.utcnow().date().isoformat() + "Z"
@@ -314,7 +310,12 @@ class Donor:
     @property
     def full_name(self):
         """ return the formal, full name """
-        return " ".join(filter(None, [self.first_name, self.middle_name, self.last_name, self.suffix]))
+        # if suffix, add with a comma
+        if self.suffix != "":
+            suffix=", " + self.suffix
+        else:
+            suffix=""    
+        return " ".join(filter(None, [self.first_name, self.middle_name, self.last_name])) + suffix
 
     @full_name.setter
     def full_name(self, full_name):
@@ -341,7 +342,11 @@ class Donor:
         everything_but_last=informal_name.split()[0:-1]
 
         # pull the first name off
-        first_name=everything_but_last.pop(0)
+        try:
+            first_name=everything_but_last.pop(0)
+        except IndexError:
+            # if we get an error, they probably gave us a malformed name
+            first_name=""
 
         try:
             # pull the middle, if exists
@@ -383,27 +388,26 @@ class Donor:
         for attr in dir(self):
             if not attr.startswith("_"):
                 which_attributes.append(attr)
-        return "DONOR: " + ", ".join(self._query_attributes(which_attributes))
-        
+        return "Donor Record ( " + ", ".join(self._query_attributes(which_attributes)) + " )"
+ 
 
-def load_donor_file(donor_file="donors.json"):
+def load_donor_file(donor_file="donors.p"):
     """ load donors from file into dict, donors """
+    donors = None
     try:
-        with open(donor_file,'r') as donor_data:
-            try:
-                donors = json.load(donor_data)
-            except (json.decoder.JSONDecodeError) as e:
-                # catch malformed json
-                print("The donors file is corrupt!")
-                print(e)
-                print("Please correct or delete the file.")
-                sys.exit(1)
-            return donors
+        donors = pickle.load( open( donor_file, "rb" ))
+        return donors
+    except (json.decoder.JSONDecodeError) as e:
+        # catch malformed json
+        print("The donors file is corrupt!")
+        print(e)
+        print("Please correct or delete the file.")
+        sys.exit(1)
     except (FileNotFoundError):
-            # if the file isn't found, that's ok, give them
-            # an empty donor dict and we'll save anything
-            # they enter upon exit.
-            return {}
+        # if the file isn't found, that's ok, give them
+        # an empty donor dict and we'll save anything
+        # they enter upon exit.
+        return donors
     except (PermissionError):
         # if the file is there, but you can't read it, don't continue
         # because you won't be able to save the file on exit, risking
@@ -413,11 +417,10 @@ def load_donor_file(donor_file="donors.json"):
         sys.exit(1)
 
 
-def save_donor_file(donors,donor_file="donors.json"):
+def save_donor_file(donors,donor_file="donors.p"):
     """ save donors to json file """
     try:
-        with open(donor_file,'w') as donor_data:
-            donor_data.write(json.dumps(donors))
+        pickle.dump( donors, open( donor_file, "wb" ))
         return True
     except (PermissionError,OSError) as e:
         print("Sorry, couldn't write the donor file!")
@@ -440,6 +443,9 @@ def safe_input(prompt=">",mock=False,mock_in=""):
         # return what the test program told you to
         return mock_in
 
+def clear_screen():
+    """ clear the terminal screen """
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def print_lines(lines=2,dest=sys.stdout):
     """ Print variable number of linefeeds for clarity. """
@@ -465,64 +471,6 @@ def list_donors(donors,dest=sys.stdout):
             donor.total_donations,
             donor.number_donations,
             donor.average_donations), file=dest)
-
-def print_report(donors,dest=sys.stdout):
-    """ Print formatted list of all donors. """
-
-    print_lines(2,dest)
-  
-    # print report header
-    #      index    name   total gvn #gifts  avg gift
-    print("{0:20} | {1:14} | {2:9} | {3:12}".format(
-        "Donor Name","Total Given","Num Gifts","Average Gift"),file=dest)
-    print("-"*72,file=dest)
-
-    for donor_id in donors:
-
-        donor=donors[donor_id]
-
-        num_donations=len(donor["donations"])
-
-        total_donations = 0
-        for donation in donor["donations"]:
-            total_donations += donation["amount"]
-
-        average_donation = total_donations / num_donations
-
-        #                 name     total gvn    #gifts    avg gift
-        print("{0:20}  ${1:14,.2f}   {2:9.0f}  ${3:12,.2f}".format(
-            donor["full_name"],
-            total_donations,
-            num_donations,
-            average_donation), file=dest)
-
-
-def parse_name(entered_name):
-        """ Convert string name into constituent parts, return dict of parts. """
-
-        # capture suffix, if present.  we assume it will follow a comma
-        try:
-            suffix=entered_name.split(",")[1].strip().upper()
-        except:
-            suffix=""
-
-        # name with suffix removed
-        informal_name=entered_name.split(",")[0].strip().title()
-
-        # we assume the last name is one word minus the suffix
-        last_name=informal_name.split()[-1].strip().title()
-       
-        # we assume the first name(s) is everything to the left of the last name
-        first_name=" ".join(informal_name.split()[0:-1]).title()
-
-        # ensure standard capitalization in suffix
-        if suffix:
-            new_full_name=", ".join([informal_name, suffix])
-        else:
-            new_full_name=informal_name
-
-        return { "full_name": new_full_name, "informal_name": informal_name,
-            "suffix": suffix, "last_name": last_name, "first_name": first_name }
 
 
 def get_donation_amount(current_donor):
@@ -557,14 +505,15 @@ def get_donation_amount(current_donor):
             print_lines()
             print("The value must be numeric.  Please try again or (q)uit.")
 
-def print_thank_you(current_donor,hint="wonderful",dest=sys.stdout):
-    """ Print a thank you letter. """
+
+def create_thank_you(donor,hint="wonderful"):
+    """ Create a thank you letter. """
 
     letter =  "\n"
-    letter += "Dearest {full_name},\n"
+    letter += "Dearest {},\n"
     letter += "\n"
     letter += "We are are grateful for the generious donation on the behalf of\n"
-    letter += "the {last_name} family.\n"
+    letter += "the {} family.\n"
     letter += "\n"
     letter += "It is through the donations of {} patrions like yourself that\n"
     letter += "allows us to continue to support the community.\n"
@@ -574,55 +523,24 @@ def print_thank_you(current_donor,hint="wonderful",dest=sys.stdout):
     letter += "Tux Humboldt\n"
     letter += "Shark Loss Prevention Institue\n"
 
+    return letter.format(donor.full_name,donor.last_name,hint)
+
+
+def print_thank_you(donor,hint="wonderful",dest=sys.stdout):
+    """ Print a thank you letter. """
+
+    letter = create_thank_you(donor,hint)
+
     print_lines(2,dest)
     print("-"*80,file=dest)
 
-    print(letter.format(hint,**current_donor),file=dest)
+    print(letter,file=dest)
 
     print("-"*80,file=dest)
     print_lines(2,dest)
 
 
-def match_donor(current_donor, donors):
-        """ See if record matches existing record, if so, return full record. """
-        for donor_id in donors:
-            existing_donor=donors[donor_id]
-            if existing_donor["full_name"].lower() == current_donor["full_name"].lower():
-                # if the owner exists, pull in the complete donor record
-                current_donor=existing_donor
-        return current_donor
-
-
-def update_donor(current_donor, donors, new_donation):
-        """ Update the donor database with new donation or donor record as needed. """
-
-        # match to existing record, if exists
-        current_donor = match_donor(current_donor, donors)
-
-        # datetime.datetime.strptime(<iso date>,'%Y-%m-%dZ')
-        today = datetime.datetime.utcnow().date().isoformat() + "Z"
-
-        # add donation to an existing donor
-        if "donor_id" in current_donor.keys():
-            donor_id=current_donor["donor_id"]
-            donors[donor_id]["donations"].append({ "amount": new_donation, "date": today })
-            hint="returning"
-        else:
-            # datetime.datetime.strptime(<iso time>,'%Y-%m-%dT%H:%M:%S.%fZ')
-            now = datetime.datetime.utcnow().isoformat() + "Z"
-            # id is a hash of the full donor name plus high resolution time
-            id_seed = current_donor["full_name"] + now
-            donor_id = hashlib.md5( id_seed.encode() ).hexdigest()
-            # add the donor
-            donors[donor_id] = { 
-                "created": now,
-                "donor_id": donor_id,
-                "donations": [ { "amount": new_donation, "date": today } ],
-                **current_donor } 
-            hint="new"
-
-
-def thank_you_entry(donors):
+def data_entry(donors):
     """ Enter new donation and send thank you. """
 
     menu =  "\n"
@@ -648,21 +566,35 @@ def thank_you_entry(donors):
 
         # check for a list directive
         if selection.lower() in ["l", "list"]:
-            print_report(donors)
+            list_donors(donors)
             continue
 
         # protect against no entry
         if not selection:
             continue
 
-        # parse the string into name components
-        current_donor=parse_name(selection)
-
         # reject blatantly bad input
-        if current_donor["first_name"] == "" or current_donor["last_name"] == "":
+        if len(selection.split()) < 2:
             print("You must enter both a first and last name.")
             continue
 
+        # find records that match the input
+        matches=donors.match_donor(selection)
+ 
+        if len(matches) == 0:
+            # if there are no matches, go ahead and create a donor record
+            current_donor=Donor(full_name=selection)
+            donors.add_donor=current_donor 
+            hint="new"
+
+        if len(matches) == 1:
+            # if there is an exact match, let's use that one
+            current_donor=donors.get_donor(matches[0][1])
+            hint="existing"
+
+        # TODO: Add confirmation logic and allow them to ignore a current record
+        #       and add a new record anyway.
+ 
         # prompt for new donation, cancel if None returned
         new_donation=get_donation_amount(current_donor)
         if new_donation is None:
@@ -671,89 +603,20 @@ def thank_you_entry(donors):
             return
 
         # update the donor list with the new donation
-        hint = update_donor(current_donor, donors, new_donation)
+        current_donor.add_donation(new_donation)
 
         # thank the donor for the new donation
         print_thank_you(current_donor,hint)
 
 
-
-def data_entry():
-    """ Enter new donation and send thank you. """
-
-    menu =  "\n"
-    menu += "DONATION ENTRY (Donor Name)\n"
-    menu += "---------------------------\n"
-    menu += "\n"
-    menu += "Enter the full name (first last) of the donor\n"
-    menu += "for whom you would like to enter a donation,\n"
-    menu += "(l)ist to see a list of the existing donors, or\n"
-    menu += "(q)uit to return to the previous menu.\n"
-    menu += "\n"
-
-    #stub it out
-    donorsx=Donors()
-
-    while True:
-
-        print_lines()
-
-        print(menu)
-        selection=safe_input("Donor Name, (l)ist or (q)uit: ")
-
-        # check for a quit directive
-        if selection.lower() in ["q", "quit"]:
-            return
-
-        # check for a list directive
-        if selection.lower() in ["l", "list"]:
-            list_donors(donorsx)
-            continue
-
-        # protect against no entry
-        if not selection:
-            continue
-
-        # reject blatantly bad input
-        # if current_donor["first_name"] == "" or current_donor["last_name"] == "":
-        #     print("You must enter both a first and last name.")
-        #     continue
-
-        # parse the string into name components
-        matches=donorsx.match_donor(selection)
- 
-        if len(matches) == 0:
-            # if there are no matches, go ahead and create a donor record
-            current_donor=Donor(full_name=selection)
-            donorsx.add_donor=current_donor 
-
-        if len(matches) == 1:
-            # if there is an exact match, let's use that one
-            current_donor=donorsx.get_donor(matches[0][1])
- 
-        # prompt for new donation, cancel if None returned
-        new_donation=get_donation_amount(current_donor)
-        if new_donation is None:
-            print_lines()
-            print("Donation cancelled!")
-            return
-
-        #print(repr(current_donor))
-
-        # update the donor list with the new donation
-        #hint = update_donor(current_donor, donors, new_donation)
-        current_donor.add_donation(new_donation)
- 
-
-        # thank the donor for the new donation
-        # print_thank_you(current_donor,hint)
-
-
 def thank_all_donors(donors,dest_override=None):
     """ loop through donors, open file, print a thank you letter. """
-    for donor_id in donors:
-        donor=donors[donor_id]
-        file_name="_".join(filter( None, [ "thank_you", donor["last_name"], donor["suffix"], donor["first_name"] ])).lower()
+
+    for name, id, _, _ in donors:
+    
+        donor=donors.get_donor(id)
+
+        file_name="_".join(filter( None, [ "thank_you", donor.last_name, donor.suffix, donor.first_name ])).lower()
         file_name=file_name.replace(" ", "_")
         if not dest_override:
             dest = open(file_name, "w")
@@ -787,28 +650,25 @@ def main(donors):
         print(menu)
         selection=safe_input("(l)ist, (e)nter, (q)uit: ").lower()
 
-        if selection in ["x"]:
-            data_entry()
-
         if selection in ["l", "list"]:
-            print_report(donors)
+            list_donors(donors)
 
         if selection in ["p", "print"]:
             thank_all_donors(donors)
 
         # accept either send or enter
         if selection in ["s", "send", "e", "enter", "a", "add"]:
-            thank_you_entry(donors)
+            data_entry(donors)
 
         if selection in ["d", "debug"]:
             print_lines()
-            pprint(donors)
+            pprint(repr(donors))
             print_lines()
 
         if selection in ["q", "quit"]:
             saved = save_donor_file(donors)
             if saved:
-                print("{} donor records saved.".format(len(donors)))
+                print("{} donor records saved.".format(len(donors.full_name_index)))
             else:
                 print("Please resolve the issues with the donor file before exiting.")
                 # if you are testing, don't get stuck in the loop
@@ -827,6 +687,8 @@ def main(donors):
 # call the main input loop
 if __name__ == "__main__":
     donors = load_donor_file()
-    main(donors)
+    if donors is None:
+        donors=Donors()
 
+    main(donors)
 
