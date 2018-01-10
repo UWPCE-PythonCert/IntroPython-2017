1d495e07cecf49aa4f05cf73d485ecc67fdc925c
Alinafe Matenda <alinafe.matenda@gmail.com>
2018-01-10 10:56:17 -0800

    Work in progress

--- students/alinafe/session10/iterator.py
+++ students/alinafe/session10/iterator.py
@@ -37,26 +37,54 @@ class IterateMe_2:
     """
 
     def __init__(self, start, stop=None, step=1):
-        self.current = -1
-        self.start = start
+        if stop == None:
+
+            self._start = 0
+            self._stop = start
+            self._step = step
+        else:
+            self._start = start
+            self._stop = stop
+            self._step = step
 
     def __iter__(self):
-        self.current = -1
-        print("in __iter__")
-        return self
+        return _RangeIter(self._start, self._stop, self._step)
+
+
+class _RangeIter:
+    def __init__(self, start, stop, step):
+        self._stop = stop
+        self._step = step
+        self._next_value = self._start
 
     def __next__(self):
-        self.current += 1
-        if self.current < self.stop:
-            return self.current
+        if self._tend():
+            raise StopIteration()
         else:
-            raise StopIteration
+            result = self._next_value
+            self._next_value += self._step
+            return result
+
+    def __iter__(self):
+        return self
+
+    def _tend(self):
+        return (self._step > 0 and self._next_value >= self._stop) or (
+                self._step < 0 and self._next_value <= self._stop)
+
 
 if __name__ == "__main__":
+
+    it = IterateMe_2(2, 20, 2)
+    for i in it:
+        if i > 10:  break
+        print(i)
+
+    for i in it:
+        print(i)
 
     print("Testing the iterator")
     it = IterateMe_2(2, 10, 2)
 
     for i in IterateMe_2(20):
         print(i)
- 
