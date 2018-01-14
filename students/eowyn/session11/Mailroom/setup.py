46e4ed967d7b765260fbb9a2dcc17ede75fa6661
Eowyn Baughman <eowyn42@gmail.com>
2018-01-14 15:45:04 -0600

    Generator labs and package homework

--- students/eowyn/session11/Mailroom/setup.py
+++ students/eowyn/session11/Mailroom/setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
 
+def get_version():
+    """
+    Reads the version string from the package __init__ and returns it
+    """
+    with open(os.path.join("capitalize", "__init__.py")) as init_file:
+        for line in init_file:
+            parts = line.strip().partition("=")
+            if parts[0].strip() == "__version__":
+                return parts[2].strip().strip("'").strip('"')
+    return None
+
+
 setup(
    name='mailroom',
-   version='0.1.0',
+   version=get_version(),
    author='Eowyn',
    author_email='aac@example.com',
    packages=['mailroom'],
