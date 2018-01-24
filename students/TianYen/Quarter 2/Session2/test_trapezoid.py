2cb4b5fd99904f3b52b67dd4f8ca885fe3f64a42
Tian Yen <tianchuanyen@hotmail.com>
2018-01-23 19:45:26 -0800

    test file for trapezoid.py with quadratic coeffiecients

--- students/TianYen/Quarter 2/Session2/test_trapezoid.py
+++ students/TianYen/Quarter 2/Session2/test_trapezoid.py
@@ -6,7 +6,7 @@ def line(x):
 
 def quad(x):
     return x**2
-    
+
 def quad2(x, A=0,B=0,C=0):
     return A * x**2 + B * x + C
 
@@ -19,5 +19,15 @@ def test_quad():
 def test_sin():
     assert math.isclose(trapz(math.sin, 0, math.pi/2), 1, rel_tol=.0001)
 
-#def test_quad_with_coefficents():
-#    assert math.isclose(trapz(quad, 0, 3, 1), 9, rel_tol=.0001)
+def test_quad_with_coefficents():
+    assert math.isclose(trapz(quad2, 0, 3, 1), 9, rel_tol=.0001)
+
+def test_quad2():
+    coef = {'A':1, 'B':3, 'C': 2}
+    assert math.isclose(trapz(quad2, 2, 20, 1,1,1), 2880, rel_tol=.0001)
+    assert math.isclose(trapz(quad2, 2, 20, 1,B=3,C=2), 3294, rel_tol=.0001)
+    assert math.isclose(trapz(quad2, 2, 20, 1,3,C=2), 3294, rel_tol=.0001)
+    assert math.isclose(trapz(quad2, 2, 20, **coef), 3294, rel_tol=.0001)
+
+#def test_sin_with_key():
+#    assert math.isclose(trapz(math.sin, 0, math.pi/2, A=4, w=2), 2(math.cos(2*0) - math.cos(pi)))
