eb04795e5646b7d3a179f12bbc564d43a59ce4f3
Kathryn Egan <kathryn.s.nichols@gmail.com>
2018-01-24 19:27:22 -0800

    thru quadratic

--- students/kegan/session13/test_trapz.py
+++ students/kegan/session13/test_trapz.py
@@ -3,6 +3,7 @@ Kathryn Egan
 """
 
 import trapz
+import math
 import pytest
 
 
@@ -40,9 +41,32 @@ def test_segment():
 def test_trapz():
     line = trapz.line(m=0, b=5)
     area = trapz.trapz(line, 0, 10)
-    assert area == 50
+    assert round(area, 5) == 50
 
     line = trapz.line(m=.5)
     area = trapz.trapz(line, 0, 10)
-    print(area)
-    assert False
+    assert round(area, 5) == 25
+
+    area = trapz.trapz(math.sin, 0, 1)
+    answer = math.cos(0) - math.cos(1)
+    assert round(area, 2) == round(answer, 2)
+
+
+# def test_arbitrary_coeffs():
+
+
+def test_polynomial():
+    A, B, C, D = 5, 4, 3, 2
+    f = trapz.polynomial(A, B, C, D)
+    x = 2
+    assert f(x) == sum([A * x ** 3, B * x ** 2, C * x, D])
+    A, B, C, D, E, F = 2, 0, 4, 1, 0, 0
+    f = trapz.polynomial(A, B, C, D, E, F)
+    x = 4
+    assert f(x) == sum([A * x ** 5, C * x ** 3, D * x ** 2])
+
+
+def test_quadtratic():
+    f = trapz.polynomial(3, 2, 2)
+    x = 5
+    assert f(x) == sum([3 * x ** 2, 2 * x, 2])
--- students/kegan/session13/trapz.py
+++ students/kegan/session13/trapz.py
@@ -3,9 +3,11 @@ Kathryn Egan
 """
 
 
-def trapz(f, a, b):
-    num_segments = 10
-    segments = [f(value) for value in segment(a, b, num_segments)]
+def trapz(f, a, b, *args, **kwargs):
+    num_segments = 1000
+    segments = [
+        f(value, *args, **kwargs)
+        for value in segment(a, b, num_segments)]
     area_sub1 = (b - a) / num_segments
     area_sub2 = (segments[0] + segments[-1]) / 2
     area_sub3 = sum(segments[1:-1])
@@ -20,6 +22,20 @@ def segment(a, b, num_segments):
 
 
 def line(m=1, b=0):
-    def line2(x):
-        return m * x + b
-    return line2
+    return polynomial(m, b)
+
+
+def quadratic(A, B, C):
+    return polynomial(A, B, C)
+
+
+def polynomial(*coeffs):
+    """ Returns function that calculates polynomial of x
+    given arbitrary number of coefficients.
+    """
+    def calculate(x):
+        powers = reversed(range(0, len(coeffs)))
+        return sum(coeff * x ** power for coeff, power in zip(coeffs, powers))
+    return calculate
+
+
