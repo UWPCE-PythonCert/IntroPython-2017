d6bd83b76dee3ed7f0e54ef20fad156c2775ae22
John Navitsky <johnn@cray.com>
2018-02-03 21:07:16 -0800

    now with moar pep8

--- students/johnn/mailroom/mailroom/audit.py
+++ students/johnn/mailroom/mailroom/audit.py
@@ -1,10 +1,24 @@
-import datetime
 """
 Add an audit entry when an object is modified.
 """
 
+import datetime
+
+
 def audit_log(called_function):
+    """
+    outside function
+
+    We expect to be passed a function and will return a function
+    (which wraps the called function)
+    """
     def audit_log_inner(self, *args, **kwargs):
+        """
+        inside function
+
+        log information to the target object about what function was called
+        and what parameters were passed.
+        """
         from mailroom import security
         if not hasattr(self, "audit"):
             self.audit = []
@@ -13,7 +27,11 @@ def audit_log(called_function):
         else:
             raise PermissionError("You must be logged in to make changes.")
         now = datetime.datetime.utcnow().isoformat() + "Z"
-        self.audit.append({ "time": now, "user": user, "action": called_function.__name__, "args": args, "kwargs": kwargs })
+        self.audit.append({"time": now,
+                           "user": user,
+                           "action": called_function.__name__,
+                           "args": args,
+                           "kwargs": kwargs})
         result = called_function(self, *args, **kwargs)
         return result
     return audit_log_inner
