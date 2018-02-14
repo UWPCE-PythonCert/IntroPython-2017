faa491e49e6f652de2d679dc32927da5c92293ce
Dan Kuchan <kuchandw@gmail.com>
2018-02-13 19:48:42 -0800

    made a few more code updates

--- students/Dkuchan/ClassProject/PythonClassProject.py
+++ students/Dkuchan/ClassProject/PythonClassProject.py
@@ -5,22 +5,55 @@
 
 
 
+
+    '''Need to add a tuple for allowed users to baseline who does what'''
+
+
+
+class Match:
+    ''' A match is a collection of data which represents a match.
+    Match is the primary class which is edited by the user.'''
+    def__init__(self):
+
+
 class Team:
-    def __init__():
+    ''' Team is an object which contains data specific to a team
+    it is not intended to be user editable but is user viewable'''
+
+    def __init__(self):
 
 
+class Roster:
+    '''class which holds a list of teams, should only be updated
+    passively or by admin'''
 
 class Competition:
-    def __init__():
+    ''' A competition is a collection of matches.  It is only intended
+    to be changed by an admin or mentor.'''
+    def __init__(self):
+
+
 
 def logmatch():
+
+
+def viewdbui():
+    '''allows the user to select options about viewing DB'''
 
 
 def updatedatabase():
 
 def importdatabase():
 
-def simulation():
+
+def setupmonte():
+    '''Allows the user to set parameters for
+    the monte carlo analysis'''
+
+
+def simulation(runs):
+    '''Runs a monte carlo with the number of runs and imported params
+    as designated in setupmonte()'''
 
 
 def primaryUI():
@@ -31,10 +64,15 @@ def primaryUI():
     print()
     print('What would you like to do?')
     print('1 - Log a match')
-    print('2 - View the database')
+    print('2 - View the Team Database')
+    print('3 - Run a Monte Carlo Analysis')
     while True:
         userin = input('Please select 1-4: ')
-        if userin = '1':
+        if userin == '1':
             logmatch()
+        elif userin == '2':
+            viewdbui()
+        elif userin == '3':
+            setupmonte()
         else:
             print('You did not select a valid option.')
