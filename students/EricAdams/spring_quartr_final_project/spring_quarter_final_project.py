02c7290a287f218045fe95a01a57d69e34293849
Eric <theadamsfamily11@msn.com>
2018-02-26 09:24:41 -0800

    state of project 2_26

--- students/EricAdams/spring_quartr_final_project/spring_quarter_final_project.py
+++ students/EricAdams/spring_quartr_final_project/spring_quarter_final_project.py
@@ -60,16 +60,19 @@ def get_column_names_in_csv_file(filename):
 # up with a list of questions to ask of the data.
 # #
 # 1. What country has the most airports with scheduled service
+# 2 What country has the most airports with unscheduled service
 
 
 def get_list_of_records_from_a_csv_file(filename):
-    """Return the records from a csv file in list form.
-    :params - filename - a .csv file.
+    """Return the records from airports.csv or countries.csv file in list form.
+    :params - filename - airport.csv or countries.csv.
     :return - the list returned will be in the form list[row][column]
     """
     with open(filename, 'r') as file_obj:
         csv_reader_obj = csv.reader(file_obj)
-        list_of_records = [record for record in csv_reader_obj]
+        # skip the first line of the file, which lists the column names
+        list_of_records = [
+            record for record in csv_reader_obj if record[0] != 'id']
     return list_of_records
 
 
@@ -93,57 +96,165 @@ def number_of_scheduled_or_nonscheduled_service_airports_per_country(scheduled_o
     for row in list_of_records_for_airports:
         list_of_countries_and_service.append([row[8], row[11]])
     for country in list_of_countries_and_service:
-        # Sample entry of country = country['JP', 'no']
-        if country[0] in number_of_airports_with_service_or_no_service_per_country:
-            number_of_airports_with_service_or_no_service_per_country[country[0]] += 1
-        else:
-            number_of_airports_with_service_or_no_service_per_country[country[0]] = 1
+        # Sample entry of country = country['JP', 'no'] or ['JP', 'yes']
+        if scheduled_or_not_scheduled is True:
+            scheduled_or_not_scheduled = "yes"
+        if scheduled_or_not_scheduled is False:
+            scheduled_or_not_scheduled = "no"
+        if country[1] == scheduled_or_not_scheduled:
+            if country[0] in number_of_airports_with_service_or_no_service_per_country:
+                number_of_airports_with_service_or_no_service_per_country[country[0]] += 1
+            else:
+                number_of_airports_with_service_or_no_service_per_country[country[0]] = 1
+    # the return will be a dict with entries like {'US':22398, ...}
     return number_of_airports_with_service_or_no_service_per_country
-    # To do list:
-    # add an "airport has service" flag
-    # country[1] is the service indicator, yes or no
-    # populate number_of_airports_with_service_or_no_service_per_country based on flag = True or False
-
 
 
 def country_with_the_most_scheduled_nonscheduled_service(scheduled_or_nonscheduled):
     """ Using the airport.csv in the current directory, return
     the country with the least or with the most scheduled service airports.
+    Also return the number of airports
     :params = Boolean.  If True, return the country with the most airports
     with scheduled service.  If False then return the country
     with the most airports without scheduled service.
+    :return - a tuple (country, number of airports with or without scheduled service)
     """
-    number_of_scheduled_nonscheduled_service_airports_in_each_country = number_of_scheduled_or_nonscheduled_service_airports_per_country(scheduled_or_nonscheduled)
-    sorted_by_value_descending = sorted(number_of_scheduled_nonscheduled_service_airports_in_each_country.items(), key=operator.itemgetter(1), reverse=True)
+    number_of_scheduled_nonscheduled_service_airports_in_each_country = number_of_scheduled_or_nonscheduled_service_airports_per_country(
+        scheduled_or_nonscheduled)
+    sorted_by_value_descending = sorted(number_of_scheduled_nonscheduled_service_airports_in_each_country.items(
+    ), key=operator.itemgetter(1), reverse=True)
+    # print(sorted_by_value_descending)
     country_code = sorted_by_value_descending[0][0]
-    return country_code, sorted_by_value_descending[00], sorted_by_value_descending[0][1]
+    country = country_code_to_country_conversion(country_code)
+    return country, sorted_by_value_descending[0][1]
 
 
 def country_code_to_country_conversion(country_code):
     '''
-    countries.csv and airport.csv are in the current directory
+    Retrieve the country from countries.csv, given the country code.
+    countries.csv must in the current directory
     country codes to country mapped in countries.csv
-    :params - country_code = country code to look up in
-    the countries.csv file
+    :params - country_code as displayed in the airports.csv file
     '''
     # list_of_records_for_countries_csv[row][1] =  countrycode
     # list_of_records_for_countries_csv[row][2] = country name
+    list_of_records = get_list_of_records_from_a_csv_file('countries.csv')
+    for record in list_of_records:
+        if country_code == record[1]:
+            return record[2]
 
 
-# 2 What country has the most airports with unscheduled service
 # 3 How many airports are within 100 miles of Seattle
-#  Some random questions
-# 1 Does the airport have a wiki page?
-# 2 What does the wiki page say?
-#     import webbrowser
-
-#     url = wikipage for airport
-#     webbrowser.open(url, new=1) #new=1 new window
-#
-# 3 Does the airport have a home link?
-# 4 What does the home link say?
 # 5 What is the lat/long of the airport?
 # 6 What state/country is the airport in?
+
+def nearby_airports_within_one_degree(airport_id, airport_lat_long=False, state_country=False):
+    """ Find other airports within a 1 deg.lat/long of airport_id.
+    :params - airport_id.  Id of the airport as shown in airports.csv
+    :return - a list of towns with airports within +- 1 deg. lat/long of airport_id.
+    """
+    # list_of_records[row][column]
+    list_of_records = get_list_of_records_from_a_csv_file('airports.csv')
+    # airport id in column 2, lat, long in column 5 and 6, airport name is in
+    # column 4, municipality in row[10]
+    # initialize airport_id_lat to determine if param is correct
+    # country = row[8]
+    # region = fow[9]
+
+    nearby_airports = []
+    country_state = []
+    for row in list_of_records:
+        # data for airport_id
+        if row[1] == airport_id:
+            airport_id_lat = row[4]
+            airport_id_long = row[5]
+            country_state.append(row[8])
+            country_state.append(row[9])
+
+            nearby_airport_max_lat = float(airport_id_lat) + 1.0
+            nearby_airport_min_lat = float(airport_id_lat) - 1.0
+            nearby_airport_max_long = float(airport_id_long) + 1.0
+            nearby_airport_min_long = float(airport_id_long) - 1.0
+    try:
+        if airport_id_lat is None:
+            pass
+    except UnboundLocalError as err:
+        print("Airport Id can not be found.\n"
+              "Make sure that"
+              "nearby_airports_within_two_degrees(airport_id)"
+              " has the correct airport_id\n", err)
+        raise
+    for row in list_of_records:
+        if row[10] not in nearby_airports:
+            if float(row[4]) <= nearby_airport_max_lat and float(row[4]) >= nearby_airport_min_lat:
+                if float(row[5]) <= nearby_airport_max_long and float(row[5]) >= nearby_airport_min_long:
+                    nearby_airports.append(row[10])
+    if airport_lat_long is False and state_country is False:
+        return nearby_airports
+    if airport_lat_long is True and state_country is False:
+        return airport_id_lat, airport_id_long
+    if airport_lat_long is False and state_country is True:
+        return country_state
+    if airport_lat_long is True and state_country is True:
+        return country_state, airport_id_lat, airport_id_long
+
+
+# To Do List:
+# search .csv's for wiki pages
+
+
+def airports_with_home_links():
+    """ Return a list of airports from the airports.csv file and their
+     home web page
+    airports.csv exists in working directory.
+    """
+    list_of_records = get_list_of_records_from_a_csv_file('airports.csv')
+    # list_of_records_for_airport_csv[row][15] = home_link
+    # list_of_records_for_airport_csv[row][16] = wiki page
+    # list_of_records_for_airport_csv[row][3] = airport name
+
+    # dict{name:homepage}
+    airport_names_homepage = {}
+    for row in list_of_records:
+        if row[3] != "name":
+            if "http" in row[15]:
+                airport_names_homepage[row[3]] = row[15]
+    return airport_names_homepage
+
+
+#  Some random questions
+# 1 Does the airport have a wiki page?
+# 2 Does the airport have a home link?
+
+def airports_with_wiki_pages():
+    """ Return a list of airports from the airports.csv file and their
+    wiki web page.
+    airports.csv exists in working directory.
+    """
+    list_of_records = get_list_of_records_from_a_csv_file('airports.csv')
+    # list_of_records_for_airport_csv[row][15] = home_link
+    # list_of_records_for_airport_csv[row][16] = wiki page
+    # list_of_records_for_airport_csv[row][3] = airport name
+
+    # dict{name:wiki}
+    airport_names_wiki = {}
+    for row in list_of_records:
+        if row[3] != "name":
+            if "http" in row[16]:
+                airport_names_wiki[row[3]] = row[16]
+    return airport_names_wiki
+
+# 3 What does the wiki page say?
+# 4 What does the home link say?
+
+
+def open_web_page(url_of_web_page):
+    """Open a web page in a new window"""
+    url = url_of_web_page
+    # new=1 opens in a new window
+    webbrowser.open(url, new=1)
+
+
 # 7 What is the field elevation?
 # 8 Can the airport land big planes?
 # 9 How long is the airport?
@@ -151,6 +262,69 @@ def country_code_to_country_conversion(country_code):
 # 11 Can a plane land at night?
 # 12 Is the runway paved?
 # 13 Is the airport even open to use?
+
+
+def runway_data_from_csv_file(airport_id, length=False, status=False, elevation=False):
+    """ Create a list of runway data, derived from runway.csv.
+    :param - airport_id = column 2 of runway.csv
+    :return - true if length is long enough for a big plane, length, if True, returns runway length
+              elevation, if True, returns runway elevation in feet above sealevel
+              status, if True, returns "closed" or "open"
+    """
+    #  Column names in runways.csv
+    # row[0] = "id",
+    # row[1] = "airport_ref",
+    # row[2] = "airport_ident",
+    # row[3] = "length_ft",
+    # row[4] = "width_ft",
+    # row[5] = "surface",
+    # row[6] = "lighted",
+    # row[7] = "closed",
+    # row[8] = "le_ident",
+    # row[9] = "le_latitude_deg",
+    # row[10] = "le_longitude_deg",
+    # row[11] = "le_elevation_ft",
+    # row[12] = "le_heading_degT",
+    # row[13] = "le_displaced_threshold_ft",
+    # row[14] = "he_ident",
+    # row[15] = "he_latitude_deg",
+    # row[16] = "he_longitude_deg",
+    # row[17] = "he_elevation_ft",
+    # row[18] = "he_heading_degT",
+    # row[19] = "he_displaced_threshold_ft",
+
+    list_of_records = get_list_of_records_from_a_csv_file('runways.csv')
+    runway_info = {}
+    for row in list_of_records:
+        # data for airport_id
+        if row[2] == airport_id:
+            runway_info['runway_elevation'] = row[11]
+            runway_info['runway_length'] = row[3]
+            runway_info['runway_width'] = row[4]
+            runway_info['runway_is_lit'] = row[6]
+            runway_info['runway_paved'] = row[5]
+            runway_info['runway_closed'] = row[7]
+    if length is False and status is False and elevation is False:
+        return runway_info
+    if length is False and status is False and elevation is True:
+        return runway_info['runway_elevation']
+    if length is False and status is True and elevation is False:
+        return runway_info['runway_closed']
+    if length is False and status is True and elevation is True:
+        return runway_info['runway_closed'], runway_info['runway_elevation']
+    if length is True and status is False and elevation is False:
+        return runway_info['runway_length']
+    if length is True and status is False and elevation is True:
+        return runway_info['runway_length'], runway_info['runway_elevation']
+    if length is True and status is True and elevation is False:
+        return runway_info['runway_length'], runway_info['runway_closed']
+    if length is True and status is True and elevation is True:
+        return runway_info['runway_length'], runway_info['runway_closed'],\
+            runway_info['runway_elevation']
+
+
+#
+
 # 14 What radio frequencies are used?
 # 15 How many navigational aids for airplanes in the country?
 # 16 Navigational aid title?
--- students/EricAdams/spring_quartr_final_project/test_spring_quarter_final_project.py
+++ students/EricAdams/spring_quartr_final_project/test_spring_quarter_final_project.py
@@ -2,62 +2,130 @@
 # test file for test_spring_quarter_final_project.py
 
 
-import csv
 from spring_quarter_final_project import download_airport_data_csv_files,\
     get_csv_files_in_current_directory, get_column_names_in_csv_file,\
     get_list_of_records_from_a_csv_file,\
     number_of_scheduled_or_nonscheduled_service_airports_per_country,\
-    country_with_the_most_scheduled_nonscheduled_service
+    country_with_the_most_scheduled_nonscheduled_service,\
+    country_code_to_country_conversion,\
+    nearby_airports_within_one_degree,\
+    airports_with_home_links,\
+    airports_with_wiki_pages,\
+    runway_data_from_csv_file
 import os
+import pytest
 
 
-# def test_download_airport_data_csv_files():
-#     url = "http://ourairports.com/data/airports.csv"
-#     filename = './airports.csv'
-#     download_airport_data_csv_files([(url, filename)])
-#     with open(filename, "r") as file_obj:
-#         result = file_obj.readline()
-#     assert '"longitude_deg"' in result
+def test_download_airport_data_csv_files():
+    url = "http://ourairports.com/data/airports.csv"
+    filename = './airports.csv'
+    download_airport_data_csv_files([(url, filename)])
+    with open(filename, "r") as file_obj:
+        result = file_obj.readline()
+    assert '"longitude_deg"' in result
 
 
-# def test_get_csv_files_in_current_directory():
-#     result = get_csv_files_in_current_directory()
-#     assert 'airports.csv' in result
+def test_get_csv_files_in_current_directory():
+    result = get_csv_files_in_current_directory()
+    assert 'airports.csv' in result
 
 
-# def test_get_column_names_in_csv_file():
-#     files = [f for f in os.listdir('.') if f.endswith('.csv')]
-#     for i in files:
-#         result = get_column_names_in_csv_file(i)
-#         assert "id" in result
+def test_get_column_names_in_csv_file():
+    files = [f for f in os.listdir('.') if f.endswith('.csv')]
+    for i in files:
+        result = get_column_names_in_csv_file(i)
+        assert "id" in result
 
 
-# def test_get_list_of_records_from_a_csv_file():
-#     results = get_list_of_records_from_a_csv_file('airports.csv')
-#     assert results[3082][9] == 'US-GA'
+def test_get_list_of_records_from_a_csv_file():
+    results = get_list_of_records_from_a_csv_file('airports.csv')
+    assert results[3082][9] == 'US-GA'
 
 
 def test_number_of_scheduled_service_airports_per_country_true():
     result = number_of_scheduled_or_nonscheduled_service_airports_per_country(True)
+    assert result['PH'] == 50
+
+
+def test_number_of_scheduled_service_airports_per_country_false():
+    result = number_of_scheduled_or_nonscheduled_service_airports_per_country(False)
+    assert result['PH'] == 229
+
+
+def test_country_with_the_most_scheduled_nonscheduled_service_most():
+    result = country_with_the_most_scheduled_nonscheduled_service(True)
+    assert result == ('United States', 479)
+    # assert False
+
+
+def test_country_with_the_most_scheduled_nonscheduled_service_least():
+    result = country_with_the_most_scheduled_nonscheduled_service(False)
+    assert result == ('United States', 21910)
+
+
+def test_country_code_to_country_conversion():
+    result = country_code_to_country_conversion('US')
+    assert result == 'United States'
+
+
+def test_nearby_airports_within_one_degree_with_invalid_param():
+    with pytest.raises(UnboundLocalError):
+        nearby_airports_within_one_degree("")
+
+
+def test_nearby_airports_within_one_degree():
+    result = nearby_airports_within_one_degree("KSEA")
+    assert 'Olympia' in result
+
+
+def test_nearby_airports_within_one_degree_state_country():
+    result = nearby_airports_within_one_degree("KSEA", airport_lat_long=False, state_country=True)
+    # print(result)
+    assert len(result) == 2
+
+
+def test_nearby_airports_within_one_degree_lat_long():
+    result = nearby_airports_within_one_degree("KSEA", airport_lat_long=True, state_country=False)
+    print(result)
+    # assert False
+    lat_long = float(result[0]) + float(result[1])
+    assert type(lat_long) == float
+
+
+def test_nearby_airports_within_one_degree_state_country_lat_long():
+    result = nearby_airports_within_one_degree("KSEA", airport_lat_long=True, state_country=True)
+    # print(result)
+    # assert False
+    assert type(result[0]) == list
+    assert type(result[1]) == str
+    assert type(result[2]) == str
+
+
+def test_airports_with_home_links():
+    result = airports_with_home_links()
+    home_link_list = list(result.values())
+    http_in_item = False
+    for item in home_link_list:
+        if 'http' in item:
+            http_in_item = True
+    assert http_in_item is True
+
+
+def test_airports_with_wiki_pages():
+    result = airports_with_wiki_pages()
+    wiki_list = list(result.values())
+    # account for some of the data being in the incorrect column
+    wiki_in_list = False
+    for item in wiki_list:
+        if 'wiki' in item:
+            wiki_in_list = True
+    assert wiki_in_list is True
+
+
+def test_runway_data_from_csv_file():
+    url = "http://ourairports.com/data/runways.csv"
+    filename = './runways.csv'
+    download_airport_data_csv_files([(url, filename)])
+    result = runway_data_from_csv_file("KSEA")
     print(result)
     assert False
-    # assert 'CF'in result
-
-
-# def test_number_of_scheduled_service_airports_per_country_false():
-#     result = number_of_scheduled_or_nonscheduled_service_airports_per_country(False)
-#     assert 'US' in result
-
-
-# def test_country_with_the_most_scheduled_nonscheduled_service_most():
-#     result = country_with_the_most_scheduled_nonscheduled_service(True)
-#     print(result)
-#     assert False
-#     # assert result == 'CN'
-
-
-# def test_country_with_the_most_scheduled_nonscheduled_service_least():
-#     result = country_with_the_most_scheduled_nonscheduled_service(False)
-#     print(result)
-#     assert False
-#     # assert result == 'JP'
