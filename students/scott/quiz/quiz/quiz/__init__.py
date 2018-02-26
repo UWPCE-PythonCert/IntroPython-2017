508330305002a97446cdc5b62d3d79f3ff17333d
Scott Peterson <scott.peterson4@gmail.com>
2018-02-26 07:14:41 -0800

    new project files

---
+++ students/scott/quiz/quiz/quiz/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''quiz package
+'''
+
+from pathlib import path
+
+__version__ = '1.0'
+
+import quiz
+
---
+++ students/scott/quiz/quiz/quiz/quiz.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+'''
+This program will allow the user to select between multiple programs,
+and the main program will allow the user to generate custom reports
+'''
+
