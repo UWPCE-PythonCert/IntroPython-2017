7dadd8d3568dc4e8fcebf1246c4aa2dffb1c4573
John Navitsky <johnn@cray.com>
2018-03-11 19:59:47 -0700

    hacking and slashing 2.0

--- students/johnn/project/dcd.py
+++ students/johnn/project/dcd.py
@@ -73,10 +73,12 @@ log.info("logging to {}".format(log_path))
 log.debug("screen log level {}".format(options.stream_log_level))
 
 def decode_command(message):
+    log.debug("admin received raw message '{}'".format(message))
     try:
         command, key, value = eval(message)
     except SyntaxError:
         command, key, value = (None, None, None)
+    log.debug("decoded message to cmd '{}', key '{}', value '{}'".format( command, key, value))
     return ( command, key, value )
 
 def blob(config):
@@ -88,12 +90,12 @@ def admin(config):
     admin.bind(options.admin_interface)
     log.info("admin bound on {}".format(options.admin_interface))
 
-    talkback = context.socket(zmq.REQ)
+    s2s_admin = context.socket(zmq.REQ)
 
     while True:
         command, key, value = decode_command(admin.recv_string())
         response = ""
-        log.info("received command {}, key {}, value {}".format(command, key, value))
+        log.info("received command '{}', key '{}', value '{}'".format(command, key, value))
         if command == "dump":
             log.debug("dump request, responding {}".format(blob(config)))
             log.info("dump request")
@@ -110,27 +112,74 @@ def admin(config):
             log.info(response)
             admin.send_string(response)
             config.peers[key]=value
+
+        if command == "sync":
+            """
+            cmd = sync, key = None, value = <server to sync to>
+            """
+            response = "ack sync to {}".format(value)
+            log.debug(response)
+            admin.send_string(response)
+            log.debug("opening connection to " + str(value))
+            s2s_admin.connect(value)
+            cmd = ("('dump', None, None)")
+            log.debug("sending command " + cmd )
+            s2s_admin.send_string(cmd)
+            message = s2s_admin.recv_string()
+            log.debug("got " + str(message) )
+            remote_ports, data, peers = eval(message)
+            # remote_admin, remote_pub = remote_ports
+            # config.peers[remote_admin] = remote_pub
+            # log.debug("remote_admin {}, remote_pub {}, data {}, peers {}".format(remote_admin, remote_pub, data, peers))
+            for topic in data:
+                log.debug("subscribing to topic {}".format(topic))
+                config.sub_queue.put(topic)
+                s2s_admin.send_string("('get', '{}', None)".format(topic))
+                message = s2s_admin.recv_string()
+                log.debug("queried key/value {}, got value {} from remote server".format(topic, message))
+                config.pub_queue.put((topic, message))
+            log.debug("asking {} to register my addresses".format(value))
+            cmd = "('register', '{}', '{}')".format(options.admin_interface, options.pub_interface)
+            log.debug("sending: " + cmd)
+            s2s_admin.send_string(cmd)
+            message = s2s_admin.recv_string()
+            # disabled since this will cause an infinite loop
+            # log.debug("asking {} to connect back to me ({})".format(value, options.admin_interface))
+            # s2s_admin.send_string("('link', None, '{}')".format(options.admin_interface))
+            # message = s2s_admin.recv_string()
+            log.debug("got {}".format(message))
+
         if command == "get":
-            log.info("getting {}".format(key))
-            try:
-                config.sub_queue.put(key)
-                value = config.get_value(key)
-                response = value
-                admin.send_string(response)
-            except KeyError:
-                config.sub_queue.put(key)
-                response = ""
-                admin.send_string(response)
+            """
+            cmd = get, key = <key to get>, value = None
+            """
+            log.debug("got cmd get")
+            response = get_value(config, key)
+            admin.send_string(response)
+            # log.info("getting {}".format(key))
+            # try:
+            #     config.sub_queue.put(key)
+            #     value = config.get_value(key)
+            #     response = value
+            #     admin.send_string(response)
+            # except KeyError:
+            #     config.sub_queue.put(key)
+            #     response = ""
+            #     admin.send_string(response)
+
         if command == "link":
+            """
+            cmd = link, key = None, value = <admin port of remote server to link to>
+            """
             response = "initiating a link request to remote admin port at {}".format(value)
             log.debug(response)
             admin.send_string(response)
             log.debug("opening connection to " + str(value))
-            talkback.connect(value)
+            s2s_admin.connect(value)
             cmd = ("('dump', None, None)")
             log.debug("sending command " + cmd )
-            talkback.send_string(cmd)  
-            message = talkback.recv_string()
+            s2s_admin.send_string(cmd)  
+            message = s2s_admin.recv_string()
             log.debug("got " + str(message) )
             remote_ports, data, peers = eval(message)
             remote_admin, remote_pub = remote_ports
@@ -139,62 +188,134 @@ def admin(config):
             for topic in data:
                 log.debug("subscribing to topic {}".format(topic))
                 config.sub_queue.put(topic)
-                talkback.send_string("('get', '{}', None)".format(topic))
-                message = talkback.recv_string()
+                s2s_admin.send_string("('get', '{}', None)".format(topic))
+                message = s2s_admin.recv_string()
                 log.debug("queried key/value {}, got value {} from remote server".format(topic, message))
                 config.pub_queue.put((topic, message))
             log.debug("asking {} to register my addresses".format(value))
             cmd = "('register', '{}', '{}')".format(options.admin_interface, options.pub_interface)
             log.debug("sending: " + cmd)
-            talkback.send_string(cmd)
-            message = talkback.recv_string()
+            s2s_admin.send_string(cmd)
+            message = s2s_admin.recv_string()
             # disabled since this will cause an infinite loop
             # log.debug("asking {} to connect back to me ({})".format(value, options.admin_interface))
-            # talkback.send_string("('link', None, '{}')".format(options.admin_interface))
-            # message = talkback.recv_string()
+            # s2s_admin.send_string("('link', None, '{}')".format(options.admin_interface))
+            # message = s2s_admin.recv_string()
             log.debug("got {}".format(message))
 
 
+def get_value(config, key):
+        """
+        given a key, get a value
+
+        cmd = get, key = <key to get>, value = None
+
+        check locally first, then query the remote servers.
+        return value or null
+        """
+        log.debug("in get_value")
+        value = ""
+        try:
+            # try getting the value locally
+            value = config.get_value(key)
+            log.info("get_value fetched value {}/{} locally".format(key, value))
+        except KeyError:
+            if config.peers:
+                log.debug("value not in local database, checking remote servers")
+                for server in config.peers:
+                    #log.debug("querying remote server {}".format(server))
+                    server_result = s2s_admin("get", key, "", server)
+                    log.debug("querying server {} for {}".format(server, key))
+                    if server_result:
+                        value = server_result
+                        log.info("got value {}/{} from server {}".format(key, value, server))
+                        # if we didn't find the value locally, but found it remotely, cache it
+                        # in our local database
+                        config.set_value(key, value)
+                        log.debug("saved {}/{} to local database".format(key, value))
+                        # subscribe to key to automatically get future updates
+                        config.sub_queue(key)
+                        log.debug("added {} to subcribe queue".format(key))
+                        # stop as soon as we get a match
+                        log.debug("terminating server search")
+                        break
+            log.debug("no remote servers, no matching entry")
+        return value
+
+
+def s2s_admin(cmd, key, value, remote_server):
+    """
+    send a command to remote server, return result
+    """
+
+    context = zmq.Context()
+    s2s_admin = context.socket(zmq.REQ)
+    message = ""
+    log.debug("opening s2s_admin connection to " + str(remote_server))
+    s2s_admin.connect(remote_server)
+    cmd = "({})".format(", ".join((cmd, key, value)))
+    log.info("sending s2s_admin command " + cmd)
+    s2s_admin.send_string(cmd)
+    message = s2s_admin.recv_string()
+    log.info("received '" + str(message) + "' from remote server" )
+    s2s_admin.disconnect(remote_server)
+    log.debug("disconnected s2s_admin connection to " + str(remote_server))
+    return message
+
+
 def pub(config):
+    """
+    publish values found on the pub_queue
+    """
     context = zmq.Context()
     pub = context.socket(zmq.PUB)
     pub.bind(options.pub_interface)
-    log.info("pub bound on " + str(options.pub_interface))
+    log.info("pub thread bound on " + str(options.pub_interface))
 
     while True:
         try:
             key, value = config.pub_queue.get()
             config.set_value(key, value)
             pub.send_string("{} {}".format(key, value))
-            log.info("published queue_entry {} {}".format(key, value))
+            log.info("published pub_queue entry {}/{}".format(key, value))
         except queue.Empty:
             continue
 
 
 def sub(config):
-    # Socket to talk to server
+    """
+    subscribe to values found on the sub_queue
+    """
     context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-
+    sub = context.socket(zmq.SUB)
     log.info("sub thread working")
 
     while True:
         try:
+            # check the queue for an entry, subscribe to what you find
             key = config.sub_queue.get()
-            log.info("noticed sub queue_entry {}".format(key))
-            socket.setsockopt_string(zmq.SUBSCRIBE, key)
+            sub.setsockopt_string(zmq.SUBSCRIBE, key)
+            log.info("subscribed to sub_queue entry {}".format(key))
+
         except queue.Empty:
+            # queue empty, nothing to do
             continue
-        try:
-            value = config.link_queue.get()
-            log.info("noticed link queue_entry {}".format(value))
-            socket.connect(value)
-        except queue.Empty:
-            continue
-        string = socket.recv_string()
-        key, value = string.split(" ", 1)
+
+        # check for subscriptions
+        subscription = sub.recv_string()
+        key, value = subscription.split(" ", 1)
+        # save value to the local database
         config.set_value( key, value )
-        log.info("sub got {} {}".format(key, value))
+        log.info("saved subsription {}/{} to local database".format(key, value))
+
+
+        # try:
+        #     value = config.link_queue.get()
+        #     log.info("noticed link queue_entry {}".format(value))
+        #     socket.connect(value)
+        # except queue.Empty:
+        #     continue
+
 
 
 class Config():
