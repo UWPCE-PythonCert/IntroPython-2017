2ba34b01f7a9897e581e57ddfc89d93b01e1740f
Tian Yen <tianchuanyen@hotmail.com>
2018-03-13 18:39:10 -0700

    updated with working labyrith creation and partial tests

--- students/TianYen/Quarter 2/Labyrinth Project/LabGame.py
+++ students/TianYen/Quarter 2/Labyrinth Project/LabGame.py
@@ -2,11 +2,12 @@ import pygame
 from pygame.locals import *
 from Labyrinth import Labyrinth, Wall
 from Player import Player
+from Text_Display import Text
 
 class App:
 
-    windowWidth = 500
-    windowHeight = 500
+    windowWidth = 650
+    windowHeight = 650
     player = 0
 
     def __init__(self):
@@ -14,8 +15,11 @@ class App:
         self._display_surf = None
         self._image_surf = None
         self._block_surf = None
-        self.player = Player()
         self.labyrinth = Labyrinth()
+        self.player = Player()
+        self.clock = pygame.time.Clock()
+        self.text = Text(self.windowWidth, self.windowHeight)
+        self.win = False
 
     def on_init(self):
         pygame.init()
@@ -23,15 +27,27 @@ class App:
 
         pygame.display.set_caption('The Labyrinth')
         self._running = True
-        self._image_surf = pygame.transform.scale(pygame.image.load("panda_player.png").convert(), (50,50))
-        self._block_surf = pygame.transform.scale(pygame.image.load("wall.png").convert(), (50,50))
+        self._image_surf = pygame.transform.scale(pygame.image.load("panda_player.png").convert(), (self.labyrinth.pixelsize,self.labyrinth.pixelsize))
+        self._block_surf = pygame.transform.scale(pygame.image.load("wall.png").convert(), (self.labyrinth.pixelsize,self.labyrinth.pixelsize))
+        self._end_rect_surf = pygame.transform.scale(pygame.image.load("end_rect.png").convert(), (self.labyrinth.pixelsize,self.labyrinth.pixelsize))
+
 
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
 
+    def on_win(self):
+        self._display_surf.fill((0,0,0))
+        self.text.display_text(self._display_surf, "You Won!", 115)
+        self.text.display_text(self._display_surf, "Would you like to keep playing? y or n", 40, self.windowWidth / 2, self.windowHeight / 1.25)
+
     def on_loop(self):
+
+
         for wall in self.labyrinth.wall_list:
+            if self.player.rect.colliderect(self.labyrinth.end_rect):
+                self.win = True
+
             if self.player.rect.colliderect(wall.rect):
 
                 #player hits the left side of the wall
@@ -51,39 +67,57 @@ class App:
                     self.player.rect.y -= self.player.speed
 
     def on_render(self):
-        self._display_surf.fill((0,0,0))
-        self._display_surf.blit(self._image_surf, (self.player.rect.x, self.player.rect.y))
-        self.labyrinth.draw(self._display_surf, self._block_surf)
+        """render the images of the labyrinth onto the screen"""
+        if self.win is True:
+            self.on_win()
+        else:
+            self._display_surf.fill((0,0,0))
+            self._display_surf.blit(self._image_surf, (self.player.rect.x, self.player.rect.y))
+            self.labyrinth.draw(self._display_surf, self._block_surf, self._end_rect_surf)
         pygame.display.flip()
 
     def on_cleanup(self):
+        """exit the game"""
+
         pygame.quit()
 
     def on_execute(self):
         if self.on_init() is False:
             self._running = False
 
-        while(self._running):
+        while self._running:
             pygame.event.pump()
             keys = pygame.key.get_pressed()
 
-            if (keys[K_RIGHT]):
+            if keys[K_RIGHT]:
                 self.player.moveRight()
 
-            if (keys[K_LEFT]):
+            elif keys[K_LEFT]:
                 self.player.moveLeft()
 
-            if (keys[K_UP]):
+            elif keys[K_UP]:
                 self.player.moveUp()
 
-            if (keys[K_DOWN]):
+            elif keys[K_DOWN]:
                 self.player.moveDown()
 
-            if (keys[K_ESCAPE]):
+            elif keys[K_ESCAPE]:
                 self._running = False
+
+            elif keys[K_y] and self.win is True:
+                self.win = False
+                self.labyrinth = Labyrinth()
+                self.player = Player()
+
+            elif keys[K_n] and self.win is True:
+                self._running = False
+
 
             self.on_loop()
             self.on_render()
+
+            self.clock.tick(120)
+
         self.on_cleanup()
 
 
--- students/TianYen/Quarter 2/Labyrinth Project/test_Labyrinth.py
+++ students/TianYen/Quarter 2/Labyrinth Project/test_Labyrinth.py
@@ -1,8 +1,19 @@
-from Labyrinth import Laby
-from LabGame import App
+from Labyrinth import Labyrinth, Wall
 
+def test_Labyrinth_init():
+    laby = Labyrinth()
 
-def test_labyrinth():
-    lab = Laby()
+def test_Wall_init():
+    wall = Wall(5,5)
+    wall2 = Wall(3,3,'north')
 
-def render_maze():
+glb_labyrinth = Labyrinth()
+
+def test_borders():
+    lab = Labyrinth(5,5)
+    assert lab.get_borders() == [[2,2,2,2,2],
+                                 [2,1,1,1,2],
+                                 [2,1,1,1,2],
+                                 [2,1,1,1,2],
+                                 [2,2,2,2,2]
+                                ]
